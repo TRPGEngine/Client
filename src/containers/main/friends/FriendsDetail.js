291df711fcf0a98eab0ecd9d8ac228a5ddcceb4f
moonrailgun <moonrailgun@gmail.com>
2017-08-31 11:48:39 +0800

    好友搜索方式搜索选单

--- src/containers/main/friends/FriendsDetail.js
+++ src/containers/main/friends/FriendsDetail.js
@@ -1,18 +1,38 @@
 const React = require('react');
 const { connect } = require('react-redux');
+const Select = require('react-select');
 
+require('react-select/dist/react-select.css');
 require('./FriendsDetail.scss');
 
 class FriendsDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectValue: 'uuid'
+    }
+  }
+
   render() {
+    let options = [
+      { value: 'uuid', label: '用户唯一标示符' },
+      { value: 'username', label: '用户名' }
+    ];
     return (
       <div className="friends-detail">
         <div className="friends-search">
           <input type="text" placeholder="请输入你要添加的好友信息" spellCheck="false" />
-          <select>
-            <option>UUID</option>
-            <option>用户名</option>
-          </select>
+          <div className="friends-search-method">
+            <Select
+              name="form-field-name"
+              value={this.state.selectValue}
+              options={options}
+              clearable={false}
+              searchable={false}
+              placeholder="请选择搜索方式..."
+              onChange={(val) => this.setState({selectValue: val})}
+            />
+          </div>
           <button>搜索</button>
         </div>
         <div className="friends-search-result">
