c352b855739e1a15f5af8ec1d39feaa5821ed451
moonrailgun <moonrailgun@gmail.com>
2017-09-16 23:45:25 +0800

    模板创建属性页面

---
+++ src/components/TemplateCell.js
@@ -0,0 +1,37 @@
+const React = require('react');
+const PropTypes = require('prop-types');
+
+require('./TemplateCell.scss');
+
+class TemplateCell extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render()　{
+    let { name, value, onEdit, onRemove } = this.props;
+
+    return (
+      <div className="template-cell">
+        <div className="property">{name} : {value}</div>
+        <div className="actions">
+          <button title="编辑" onClick={onEdit}>
+            <i className="iconfont">&#xe83f;</i>
+          </button>
+          <button title="删除" onClick={onRemove}>
+            <i className="iconfont">&#xe76b;</i>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+TemplateCell.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onEdit: PropTypes.func,
+  onRemove: PropTypes.func,
+}
+
+module.exports = TemplateCell;
--- src/containers/main/actors/TemplateCreate.js
+++ src/containers/main/actors/TemplateCreate.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const { connect } = require('react-redux')
 const { showModal } = require('../../../redux/actions/ui');
+const TemplateCell = require('../../../components/TemplateCell');
 
 require('./TemplateCreate.scss');
 
@@ -17,8 +18,34 @@ class TemplateCreate extends React.Component {
   render()　{
     return (
       <div className="template-create">
-        <div className="header">
-          <button onClick={() => this._handleBack()}>返回</button>
+        <div className="profile-panel">
+          <div className="header">
+            <button onClick={() => this._handleBack()}>&lt;返回</button>
+            <input placeholder="模板名" />
+            <button>保存</button>
+          </div>
+          <div className="desc">
+            <textarea placeholder="模板描述" rows="4" cols="60"></textarea>
+          </div>
+          <div className="property-list">
+            <TemplateCell name="力量" value="20" />
+            <TemplateCell name="敏捷" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+            <TemplateCell name="体质" value="20"/>
+          </div>
+        </div>
+        <div className="inspect-panel">
+
         </div>
       </div>
     )
