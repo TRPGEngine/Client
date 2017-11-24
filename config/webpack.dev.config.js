f05ac00767393336dcd54633a36b64e3959fbd9d
moonrailgun <moonrailgun@gmail.com>
2017-11-24 08:51:07 +0800

    webpack 配置

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,15 @@
+const webpackMerge = require('webpack-merge');
+const base = require('./webpack.base.config.js');
+
+module.exports = webpackMerge({}, base, {
+  devtool: 'cheap-module-eval-source-map', //开发环境
+
+  devServer: {
+    compress: true, // 启用Gzip压缩
+    historyApiFallback: true, // 为404页启用多个路径
+    hot: true, // 模块热更新，配置HotModuleReplacementPlugin
+    https: false, // 适用于ssl安全证书网站
+    noInfo: true, // 只在热加载错误和警告
+    // ...
+  },
+})
---
+++ config/webpack.prod.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const base = require('./webpack.base.config.js');
+
+module.exports = webpackMerge({}, base, {
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_debugger: true,
+        drop_console: true
+      },
+      sourceMap: true
+    }),
+  ]
+})
