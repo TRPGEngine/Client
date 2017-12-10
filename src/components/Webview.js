c27014bf77fafc2ec9266795415eb4bd5e4dc250
moonrailgun <moonrailgun@gmail.com>
2017-12-10 15:47:27 +0800

    使用webview替换electron环境下的嵌入网页组件

--- src/components/Webview.js
+++ src/components/Webview.js
@@ -1,11 +1,13 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const NProgress = require('nprogress');
+const config = require('../../config/project.config');
 
 require('nprogress/nprogress.css');
 require('./Webview.scss');
 
 let webframeIndex = 0;
+const isElectron = config.platform === 'electron';
 
 class Webview extends React.Component {
   constructor(props) {
@@ -17,17 +19,35 @@ class Webview extends React.Component {
   componentDidMount() {
     NProgress.configure({parent: "#"+this.id})
     NProgress.start();
-    this.refs.webframe.src = this.props.src;
-    this.refs.webframe.onload = function(e) {
-      console.log('webview loadCompleted');
-      NProgress.done();
+    if(isElectron) {
+      this.refs.webframe.src = this.props.src;
+      this.refs.webframe.addEventListener('dom-ready', function() {
+        console.log('webview loadCompleted');
+        NProgress.done();
+      })
+      this.refs.webframe.addEventListener('will-navigate', function(e) {
+        console.log('webview change, url:', e.url);
+        NProgress.start();
+      })
+    }else {
+      this.refs.webframe.src = this.props.src;
+      this.refs.webframe.onload = function(e) {
+        console.log('webview loadCompleted');
+        NProgress.done();
+      }
     }
   }
 
   render() {
     return (
       <div className="webview" id={this.id}>
-        <iframe ref="webframe" />
+        {
+          isElectron ? (
+            <webview ref="webframe" />
+          ) : (
+            <iframe ref="webframe" />
+          )
+        }
       </div>
     )
   }
