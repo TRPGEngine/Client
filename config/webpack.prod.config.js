911db8f244e6dff1be2a23368a965f030a42ca7f
moonrailgun <moonrailgun@gmail.com>
2017-12-26 10:35:04 +0800

    修改编译顺序的问题

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -3,17 +3,17 @@ const webpackMerge = require('webpack-merge');
 const base = require('./webpack.base.config.js');
 
 module.exports = webpackMerge({}, base, {
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compress: {
-  //       warnings: false,
-  //       drop_debugger: true,
-  //       drop_console: true,
-  //     },
-  //     sourceMap: false,
-  //     mangle: {
-  //       except: ['$super', '$', 'exports', 'require'], // 排除关键字
-  //     }
-  //   }),
-  // ]
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_debugger: true,
+        drop_console: true,
+      },
+      sourceMap: false,
+      mangle: {
+        except: ['$super', '$', 'exports', 'require'], // 排除关键字
+      }
+    }),
+  ]
 })
