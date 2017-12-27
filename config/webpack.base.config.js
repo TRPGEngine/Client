6ecc9d9b0c31df2f88e83978c2aff0b492c38f39
moonrailgun <moonrailgun@gmail.com>
2017-12-27 09:50:07 +0800

    删除一段无用代码

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -37,17 +37,6 @@ module.exports = {
     electron: "require('electron')",
   },
 
-  // devtool: 'cheap-module-eval-source-map', //开发环境
-  //
-  // devServer: {
-  //   compress: true, // 启用Gzip压缩
-  //   historyApiFallback: true, // 为404页启用多个路径
-  //   hot: true, // 模块热更新，配置HotModuleReplacementPlugin
-  //   https: false, // 适用于ssl安全证书网站
-  //   noInfo: true, // 只在热加载错误和警告
-  //   // ...
-  // },
-
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
