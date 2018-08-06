e521cc047e5c701cace293ddf225950dd2954f9f
moonrailgun <moonrailgun@gmail.com>
2018-08-06 14:21:29 +0800

    优化对话界面进度条滚动处理

--- src/web/components/MsgContainer.js
+++ src/web/components/MsgContainer.js
@@ -15,8 +15,6 @@ class MsgContainer extends React.Component {
   }
 
   componentDidMount() {
-    scrollTo.bottom(this.refs.container, 400);
-
     if(this.props.msgList.size === 0) {
       this.setState({nomore: true});
     }else {
@@ -24,22 +22,11 @@ class MsgContainer extends React.Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     if(nextProps.msgList.size === 0 || nextProps.msgList.size === this.props.msgList.size) {
       this.setState({nomore: true});
     }else {
       this.setState({nomore: false});
-    }
-
-    if(
-      nextProps.converseUUID !== this.props.converseUUID ||
-      nextProps.msgList.size === 0 || this.props.msgList.size === 0 ||
-      nextProps.msgList.last().get('date') !== this.props.msgList.last().get('date')
-    ) {
-      // 界面切换
-      setTimeout(() => {
-        scrollTo.bottom(this.refs.container, 100);
-      }, 0);
     }
 
     if(
@@ -59,6 +46,13 @@ class MsgContainer extends React.Component {
     let date = this.props.msgList.first().get('date');
     let { converseUUID } = this.props;
     this.props.dispatch(getMoreChatLog(converseUUID, date, !this.props.isGroup));
+  }
+
+  _handleContainerLoad() {
+    // 进度条滚动到底部
+    setTimeout(() => {
+      scrollTo.bottom(this.refs.container, 100);
+    }, 0);
   }
 
   prepareMsgItemCardData(data) {
@@ -117,7 +111,11 @@ class MsgContainer extends React.Component {
     let { userUUID, usercache } = this.props;
 
     return (
-      <div className={'msg-container ' + this.props.className} ref="container">
+      <div
+        className={'msg-container ' + this.props.className}
+        ref="container"
+        onLoad={() => this._handleContainerLoad()}
+      >
         {
           this.state.nomore || this.props.msgList.size < 10 ? (
             <button className="get-more-log-btn" disabled={true} style={{display: this.props.msgList.size < 10 ? 'none':'block'}}>没有更多记录了</button>
