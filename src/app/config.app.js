b6f48791d8a37b899159a76e4ebab6754e6e8663
moonrailgun <moonrailgun@gmail.com>
2018-08-24 17:24:45 +0800

    WIP:增加动态默认图片

--- src/app/config.app.js
+++ src/app/config.app.js
@@ -1,6 +1,11 @@
+const config = require('../../config/project.config');
+
 module.exports = {
   defaultImg: {
     user: require('../assets/img/gugugu1.png'),
+    getUser (name) {
+      return `${config.file.url}/file/avatar/svg?name=${name || ''}`
+    },
     group: require('../assets/img/gugugu1.png'),
     trpgsystem: require('../assets/img/system_notice.png'),
     actor: null,
