7cf9993a9c5dfa11d0f415c632211de08d7f83c7
moonrailgun <moonrailgun@gmail.com>
2018-08-30 11:53:22 +0800

    TPopup.js

---
+++ src/app/components/TApi/TPopup.js
@@ -0,0 +1,49 @@
+const React = require('react');
+const {
+  View
+} = require('react-native');
+import RootSiblings from 'react-native-root-siblings';
+
+const styles = {
+  container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+}
+
+class TPopupContainer extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.props.children}
+      </View>
+    )
+  }
+}
+
+const TPopup = {
+  _window: null,
+  show: function(win) {
+    if(this._window) {
+      this.hide();
+    }
+
+    this._window = new RootSiblings(
+      <TPopupContainer>
+        {win}
+      </TPopupContainer>
+    );
+    return this._window;
+  },
+  hide: function() {
+    if(this._window && this._window.destroy) {
+      this._window.destroy();
+      this._window = null;
+    }
+  }
+}
+
+module.exports = TPopup;
