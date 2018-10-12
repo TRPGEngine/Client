52ed5fdeb3973ca78ab200f817fbc922f4f47866
moonrailgun <moonrailgun@gmail.com>
2018-10-12 15:33:29 +0800

    增加入团申请卡片消息

--- src/web/components/messageTypes/Card.js
+++ src/web/components/messageTypes/Card.js
@@ -4,6 +4,7 @@ const Base = require('./Base');
 const CardType = {
   'default': require('./card/DefaultCard'),
   'friendInvite': require('./card/FriendInvite'),
+  'groupRequest': require('./card/GroupRequest'),
   'groupInvite': require('./card/GroupInvite'),
   'diceRequest': require('./card/DiceRequest'),
   'diceInvite': require('./card/DiceInvite'),
@@ -15,7 +16,6 @@ class Card extends Base {
     let info = this.props.info;
     let data = info.data;
     let Card = CardType[data.type] || CardType['default'];
-    console.log('TODO: card info and props', info, this.props); // TODO: 需要实现各个类型的card方法
 
     return (
       <Card {...this.props} />
