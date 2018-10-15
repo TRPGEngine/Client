c7c687450d5d85a7d79d6c38f847282e09448573
moonrailgun <moonrailgun@gmail.com>
2018-10-15 16:20:59 +0800

    彻底弃用web端MsgItem

--- src/web/components/MsgContainer.js
+++ src/web/components/MsgContainer.js
@@ -1,6 +1,5 @@
 const React = require('react');
 const { connect } = require('react-redux');
-const MsgItem = require('./MsgItem');
 const config = require('../../../config/project.config.js');
 const dateHelper = require('../../shared/utils/dateHelper');
 const scrollTo = require('../../shared/utils/animatedScrollTo.js');
@@ -10,6 +9,7 @@ const MessageHandler = require('../../shared/component/MessageHandler');
 MessageHandler.registerDefaultMessageHandler(require('./messageTypes/Default'));
 MessageHandler.registerMessageHandler('tip', require('./messageTypes/Tip'));
 MessageHandler.registerMessageHandler('card', require('./messageTypes/Card'));
+require('./messageTypes/MsgItem.scss');
 
 require('./MsgContainer.scss');
 class MsgContainer extends React.Component {
@@ -88,58 +88,6 @@ class MsgContainer extends React.Component {
     }, 0);
   }
 
-  prepareMsgItemCardData(data) {
-    if(data.get('type') === 'friendInvite') {
-      let inviteUUID = data.getIn(['invite', 'uuid']);
-      let from_uuid = data.getIn(['invite', 'from_uuid']);
-      let inviteIndex = this.props.friendRequests.findIndex((item) => {
-        if(item.get('uuid') === inviteUUID) {
-          return true
-        }else {
-          return false
-        }
-      });
-      if(inviteIndex >= 0) {
-        // 尚未处理
-        data = data.set('actionState', 0);
-      }else {
-        let friendIndex = this.props.friendList.indexOf(from_uuid);
-        if(friendIndex >= 0) {
-          // 已同意是好友
-          data = data.set('actionState', 1);
-        }else {
-          // 已拒绝好友邀请
-          data = data.set('actionState', 2);
-        }
-      }
-    }else if(data.get('type') === 'groupInvite') {
-      let inviteUUID = data.getIn(['invite', 'uuid']);
-      let group_uuid = data.getIn(['invite', 'group_uuid']);
-      let inviteIndex = this.props.groupInvites.findIndex((item) => {
-        if(item.get('uuid') === inviteUUID) {
-          return true
-        }else {
-          return false
-        }
-      });
-      if(inviteIndex >= 0) {
-        // 尚未处理
-        data = data.set('actionState', 0);
-      }else {
-        let friendIndex = this.props.groupUUIDList.indexOf(group_uuid);
-        if(friendIndex >= 0) {
-          // 已同意是邀请
-          data = data.set('actionState', 1);
-        }else {
-          // 已拒绝邀请
-          data = data.set('actionState', 2);
-        }
-      }
-    }
-
-    return data;
-  }
-
   render() {
     let { userUUID, usercache } = this.props;
 
@@ -161,7 +109,6 @@ class MsgContainer extends React.Component {
           {
             this.props.msgList.map((item, index, arr) => {
               const prevDate = index > 0 ? arr.getIn([index - 1, 'date']) : 0;
-              let data = item.get('data');
               let isMe = userUUID===item.get('sender_uuid');
               let avatar = isMe ? this.props.selfInfo.get('avatar') : usercache.getIn([item.get('sender_uuid'), 'avatar'])
               let name = isMe
@@ -169,11 +116,6 @@ class MsgContainer extends React.Component {
                 : usercache.getIn([item.get('sender_uuid'), 'nickname']) || usercache.getIn([item.get('sender_uuid'), 'username']);
               let defaultAvatar = item.get('sender_uuid') === 'trpgsystem' ? config.defaultImg.trpgsystem : config.defaultImg.getUser(name);
               let date = item.get('date');
-
-              // data 预处理
-              if(data && item.get('type') === 'card') {
-                data = this.prepareMsgItemCardData(data);
-              }
 
               let diffTime = dateHelper.getDateDiff(prevDate, date);
               let emphasizeTime = diffTime / 1000 / 60 >= 10 // 超过10分钟
@@ -187,23 +129,6 @@ class MsgContainer extends React.Component {
                   avatar={avatar || defaultAvatar}
                   emphasizeTime={emphasizeTime}
                   info={item.toJS()}
-                />
-              )
-
-              // TODO
-              return (
-                <MsgItem
-                  key={item.get('uuid')+'+'+index}
-                  uuid={item.get('uuid')}
-                  icon={icon || defaultAvatar}
-                  name={name || ''}
-                  type={item.get('type')}
-                  content={item.get('message')}
-                  data={data}
-                  time={date}
-                  me={isMe}
-                  isGroupMsg={false}
-                  emphasizeTime={emphasizeTime}
                 />
               )
             })
