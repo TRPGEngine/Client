d3951eed89e4c85a97728f09a6f083bf18b0b3d9
moonrailgun <moonrailgun@gmail.com>
2018-11-08 10:53:20 +0800

    :star2: 增加文件图标图片

--- src/app/config.app.js
+++ src/app/config.app.js
@@ -6,6 +6,15 @@ module.exports = {
     group: require('../assets/img/gugugu1.png'),
     trpgsystem: require('../assets/img/system_notice.png'),
     actor: null,
+    file: {
+      default: require('../assets/img/file/default.png'),
+      pdf: require('../assets/img/file/pdf.png'),
+      excel: require('../assets/img/file/excel.png'),
+      ppt: require('../assets/img/file/ppt.png'),
+      word: require('../assets/img/file/word.png'),
+      txt: require('../assets/img/file/txt.png'),
+      pic: require('../assets/img/file/pic.png'),
+    },
   },
   oauth: {
     qq: {
