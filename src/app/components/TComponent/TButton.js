358f02ef90ce6b1bb1043019d2543c413f6f5bd6
moonrailgun <moonrailgun@gmail.com>
2018-11-14 10:22:54 +0800

    :hammer: 重新调整TButton的相关设置

--- src/app/components/TComponent/TButton.js
+++ src/app/components/TComponent/TButton.js
@@ -1,14 +1,19 @@
 const React = require('react');
 const Button = require('apsl-react-native-button');
+const {
+  TouchableOpacity,
+  TouchableNativeFeedback,
+} = require('react-native');
 
 class TButton extends React.Component {
   render() {
     return (
       <Button
-        textStyle={{color: 'white'}}
         {...this.props}
+        textStyle={[textStyles.default, textStyles[this.props.type], this.props.textStyle]}
         style={[styles.default, styles[this.props.type], this.props.style]}
-      />
+        isDisabled={this.props.disabled}
+      >{this.props.children}</Button>
     )
   }
 }
@@ -22,6 +27,18 @@ const styles = {
   error: {
     backgroundColor: '#ff4400',
   },
+  inline: {
+    backgroundColor: 'transparent',
+  },
+}
+
+const textStyles = {
+  'default': {
+    color: 'white',
+  },
+  inline: {
+    color: '#705949',
+  }
 }
 
 module.exports = TButton;
