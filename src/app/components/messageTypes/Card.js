c090c72ca9b229cb08acddce2240a18432ee07a0
moonrailgun <moonrailgun@gmail.com>
2019-01-22 20:01:33 +0800

    :star: app端人物卡消息列表

--- src/app/components/messageTypes/Card.js
+++ src/app/components/messageTypes/Card.js
@@ -8,7 +8,7 @@ const CardType = {
   'groupInvite': require('./card/GroupInvite'),
   'diceRequest': require('./card/DiceRequest'),
   'diceInvite': require('./card/DiceInvite'),
-  // 'actor': require('./card/Actor'),
+  'actor': require('./card/Actor'),
 }
 
 class Card extends Base {
