c3a1c61ff9981d24fd7bcceaee4bebc262f30925
moonrailgun <moonrailgun@gmail.com>
2019-03-16 21:06:52 +0800

    :hammer: loading组件拆分

--- src/web/components/Loading.js
+++ src/web/components/Loading.js
@@ -1,5 +1,7 @@
-const React = require('react');
-const PropTypes = require('prop-types');
+import React from 'react';
+import PropTypes from 'prop-types';
+import LoadingSpinner from './LoadingSpinner';
+
 require('./Loading.scss');
 
 class Loading extends React.Component {
@@ -9,13 +11,7 @@ class Loading extends React.Component {
       body = (
         <div className="mask">
           <div className="content">
-            <div className="spinner">
-              <div className="rect1" />
-              <div className="rect2" />
-              <div className="rect3" />
-              <div className="rect4" />
-              <div className="rect5" />
-            </div>
+            <LoadingSpinner />
             <span>{this.props.text}</span>
           </div>
         </div>
