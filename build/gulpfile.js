e3d86d2c66f38fe17699b51b99ff125e2fbbbc0e
moonrailgun <moonrailgun@gmail.com>
2019-03-17 15:47:24 +0800

    :fire: gulp 3->4

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -2,21 +2,15 @@ const gulp = require('gulp');
 const replace = require('gulp-replace');
 const log = require('fancy-log');
 const PluginError = require('plugin-error');
-
-gulp.task('default', ['assets', 'webpack', 'redirect']);
+const {
+  series, //顺序
+  parallel, // 并行
+} = gulp;
 
 gulp.task('assets', function() {
   gulp.src('../src/assets/**/*').pipe(gulp.dest('../dist/assets/'));
 
   gulp.src('../build/entry.js').pipe(gulp.dest('../dist/'));
-});
-
-gulp.task('redirect', ['webpack'], function() {
-  gulp
-    .src('../dist/bundle.js')
-    .pipe(replace('/src/assets/', './assets/'))
-    .pipe(replace('//at.alicdn.com', 'https://at.alicdn.com'))
-    .pipe(gulp.dest('../dist/'));
 });
 
 gulp.task('webpack', function(callback) {
@@ -31,36 +25,55 @@ gulp.task('webpack', function(callback) {
   });
 });
 
-gulp.task('package', ['assets', 'webpack', 'redirect'], function(callback) {
-  const packager = require('electron-packager');
-  const packagerConfig = require('../config/packager.config.js');
-  log('[electron-packager]', 'start packing...');
+gulp.task(
+  'redirect',
+  series('webpack', function() {
+    gulp
+      .src('../dist/bundle.js')
+      .pipe(replace('/src/assets/', './assets/'))
+      .pipe(replace('//at.alicdn.com', 'https://at.alicdn.com'))
+      .pipe(gulp.dest('../dist/'));
+  })
+);
 
-  packager(packagerConfig, function(err, appPaths) {
-    if (err) {
-      throw new Error(err);
-    } else {
-      log('[electron-packager]', 'package completed!', appPaths);
-      callback();
-    }
-  });
-});
+gulp.task('default', parallel('assets', 'webpack', 'redirect'));
 
-gulp.task('package:all', ['assets', 'webpack', 'redirect'], function(callback) {
-  const packager = require('electron-packager');
-  const packagerConfig = require('../config/packager.config.js');
-  packagerConfig.all = true;
-  log('[electron-packager]', 'start packing...');
+gulp.task(
+  'package',
+  series('default', function(callback) {
+    const packager = require('electron-packager');
+    const packagerConfig = require('../config/packager.config.js');
+    log('[electron-packager]', 'start packing...');
 
-  packager(packagerConfig, function(err, appPaths) {
-    if (err) {
-      throw new Error(err);
-    } else {
-      log('[electron-packager]', 'package completed!', appPaths);
-      callback();
-    }
-  });
-});
+    packager(packagerConfig, function(err, appPaths) {
+      if (err) {
+        throw new Error(err);
+      } else {
+        log('[electron-packager]', 'package completed!', appPaths);
+        callback();
+      }
+    });
+  })
+);
+
+gulp.task(
+  'package:all',
+  series('default', function(callback) {
+    const packager = require('electron-packager');
+    const packagerConfig = require('../config/packager.config.js');
+    packagerConfig.all = true;
+    log('[electron-packager]', 'start packing...');
+
+    packager(packagerConfig, function(err, appPaths) {
+      if (err) {
+        throw new Error(err);
+      } else {
+        log('[electron-packager]', 'package completed!', appPaths);
+        callback();
+      }
+    });
+  })
+);
 
 // 将字体文件复制到设备对应目录
 gulp.task('copyIconfont', function() {
