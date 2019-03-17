a77d5c1a3877bd6934e80629f70ca10b6de34d97
moonrailgun <moonrailgun@gmail.com>
2019-03-17 14:21:00 +0800

    :hammer: fix deprecated gulp util

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,32 +1,32 @@
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const replace = require('gulp-replace');
+const log = require('fancy-log');
+const PluginError = require('plugin-error');
 
 gulp.task('default', ['assets', 'webpack', 'redirect']);
 
 gulp.task('assets', function() {
-  gulp.src('../src/assets/**/*')
-    .pipe(gulp.dest('../dist/assets/'))
+  gulp.src('../src/assets/**/*').pipe(gulp.dest('../dist/assets/'));
 
-  gulp.src('../build/entry.js')
-    .pipe(gulp.dest('../dist/'))
-})
+  gulp.src('../build/entry.js').pipe(gulp.dest('../dist/'));
+});
 
 gulp.task('redirect', ['webpack'], function() {
-  gulp.src('../dist/bundle.js')
+  gulp
+    .src('../dist/bundle.js')
     .pipe(replace('/src/assets/', './assets/'))
     .pipe(replace('//at.alicdn.com', 'https://at.alicdn.com'))
-    .pipe(gulp.dest('../dist/'))
-})
+    .pipe(gulp.dest('../dist/'));
+});
 
 gulp.task('webpack', function(callback) {
   const webpack = require('webpack');
   const webpackConfig = require('../config/webpack.config.js');
-  webpack(webpackConfig, function (err, stats) {
+  webpack(webpackConfig, function(err, stats) {
     if (err) {
-      throw new gutil.PluginError('webpack', err);
+      throw new PluginError('webpack', err);
     }
-    gutil.log('[webpack]', stats.toString({ modules: false, colors: true }));
+    log('[webpack]', stats.toString({ modules: false, colors: true }));
     callback();
   });
 });
@@ -34,37 +34,38 @@ gulp.task('webpack', function(callback) {
 gulp.task('package', ['assets', 'webpack', 'redirect'], function(callback) {
   const packager = require('electron-packager');
   const packagerConfig = require('../config/packager.config.js');
-  gutil.log('[electron-packager]', 'start packing...');
+  log('[electron-packager]', 'start packing...');
 
   packager(packagerConfig, function(err, appPaths) {
-    if(err) {
+    if (err) {
       throw new Error(err);
-    }else {
-      gutil.log('[electron-packager]', 'package completed!', appPaths);
+    } else {
+      log('[electron-packager]', 'package completed!', appPaths);
       callback();
     }
-  })
-})
+  });
+});
 
 gulp.task('package:all', ['assets', 'webpack', 'redirect'], function(callback) {
   const packager = require('electron-packager');
   const packagerConfig = require('../config/packager.config.js');
   packagerConfig.all = true;
-  gutil.log('[electron-packager]', 'start packing...');
+  log('[electron-packager]', 'start packing...');
 
   packager(packagerConfig, function(err, appPaths) {
-    if(err) {
+    if (err) {
       throw new Error(err);
-    }else {
-      gutil.log('[electron-packager]', 'package completed!', appPaths);
+    } else {
+      log('[electron-packager]', 'package completed!', appPaths);
       callback();
     }
-  })
-})
+  });
+});
 
 // 将字体文件复制到设备对应目录
 gulp.task('copyIconfont', function() {
-  return gulp.src('../src/assets/fonts/iconfont.ttf')
+  return gulp
+    .src('../src/assets/fonts/iconfont.ttf')
     .pipe(gulp.dest('../android/app/src/main/assets/fonts/'))
-    .pipe(gulp.dest('../ios/trpg/Images.xcassets/iconfont.dataset/'))
-})
+    .pipe(gulp.dest('../ios/trpg/Images.xcassets/iconfont.dataset/'));
+});
