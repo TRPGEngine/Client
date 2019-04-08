554b3ea61da2942359ad2be5ab9367fd3e52fe4d
moonrailgun <moonrailgun@gmail.com>
2019-04-08 17:38:08 +0800

    :hammer: 调整prepare_env脚本并增加缓存文件夹

--- .circleci/prepare_env.sh
+++ .circleci/prepare_env.sh
@@ -6,11 +6,10 @@ case "$OSTYPE" in
   "linux-gnu")
     # Not using Trusty containers because it can't install wine1.6(-i386),
     # see: https://github.com/travis-ci/travis-ci/issues/6460
-    sudo rm /etc/apt/sources.list.d/google-chrome.list
     sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
     echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo dpkg --add-architecture i386
     sudo apt-get update
-    sudo apt-get install -y wine1.6 yarn
+    sudo apt-get install -y wine yarn
     ;;
 esac
