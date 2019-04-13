70b0fb3af811b26428ce8ba04d9667e89fa84864
moonrailgun <moonrailgun@gmail.com>
2019-04-14 00:14:22 +0800

    :star: 增加打包分析报告
    用法:
    npm run build:report 或 npm run build:pro --report

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const _ = require('lodash');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
-const SentryCliPlugin = require('@sentry/webpack-plugin');
 const base = require('./webpack.base.config.js');
 const package = require('../package.json');
 const config = require('config');
@@ -13,6 +12,7 @@ const APP_PATH = path.resolve(ROOT_PATH, 'src');
 const plugins = [];
 if (_.get(config, 'sentry.pushRelease', false) === true) {
   // 增加推送插件
+  const SentryCliPlugin = require('@sentry/webpack-plugin');
   plugins.push(
     new SentryCliPlugin({
       include: APP_PATH,
@@ -20,6 +20,20 @@ if (_.get(config, 'sentry.pushRelease', false) === true) {
       ignore: ['app'],
       configFile: 'sentry.properties',
       release: `v${package.version}-${process.env.NODE_ENV}`,
+    })
+  );
+}
+
+// use npm run build:report or npm run build:pro --report
+// to generate bundle-report
+if (_.get(process, 'env.npm_config_report', false)) {
+  const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+  plugins.push(
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'bundle-report.html',
+      openAnalyzer: true,
     })
   );
 }
