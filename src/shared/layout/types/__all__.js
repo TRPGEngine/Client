8c9726209bfcc8907bf8dc0d9f72082eb1f1730a
moonrailgun <moonrailgun@gmail.com>
2019-04-16 19:36:48 +0800

    :star: add col component

--- src/shared/layout/types/__all__.js
+++ src/shared/layout/types/__all__.js
@@ -1,8 +1,10 @@
+import Col from './Col.tsx';
 import Define from './Define.tsx';
 import Input from './Input.tsx';
 import Row from './Row.tsx';
 import { register } from '.';
 
+register(Col);
 register(Define);
 register(Input);
 register(Row);
