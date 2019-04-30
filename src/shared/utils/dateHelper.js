b64501d310a57a1bd1f40ab596382c28d1fc8c66
moonrailgun <moonrailgun@gmail.com>
2019-04-30 10:49:35 +0800

    :hammer: 修复dateHelper的引用

--- src/shared/utils/dateHelper.js
+++ src/shared/utils/dateHelper.js
@@ -1,44 +1,52 @@
 import moment from 'moment';
 
-let helper = {
-  getShortDiff: (str) => {
-    let date = str ? moment(str) : moment();
-    if (helper.isToday(date)) {
-      return date.format('HH:mm');
-    } else {
-      return date.fromNow();
-    }
-  },
-  getShortDate: (str) => {
-    let date = str ? moment(str) : moment();
-    if (helper.isToday(date)) {
-      return date.format('A hh:mm');
-    } else {
-      return date.format('MM-DD HH:mm');
-    }
-  },
-  getMsgDate: (str) => {
-    let date = str ? moment(str) : moment();
-    if (helper.isToday(date)) {
-      return date.format('HH:mm:ss');
-    } else {
-      return date.format('YYYY-MM-DD HH:mm:ss');
-    }
-  },
-  getFullDate: (str) => {
-    let date = str ? moment(str) : moment();
+export const getShortDiff = (str) => {
+  let date = str ? moment(str) : moment();
+  if (helper.isToday(date)) {
+    return date.format('HH:mm');
+  } else {
+    return date.fromNow();
+  }
+};
+export const getShortDate = (str) => {
+  let date = str ? moment(str) : moment();
+  if (helper.isToday(date)) {
+    return date.format('A hh:mm');
+  } else {
+    return date.format('MM-DD HH:mm');
+  }
+};
+export const getMsgDate = (str) => {
+  let date = str ? moment(str) : moment();
+  if (helper.isToday(date)) {
+    return date.format('HH:mm:ss');
+  } else {
     return date.format('YYYY-MM-DD HH:mm:ss');
-  },
-  getSamlpeDate: (str) => {
-    let date = str ? moment(str) : moment();
-    return date.format('YYYY-MM-DD');
-  },
-  isToday: (date) => {
-    return moment().isSame(date, 'day');
-  },
-  getDateDiff: (prevDate, nextDate) => {
-    return new Date(nextDate) - new Date(prevDate);
-  },
+  }
+};
+export const getFullDate = (str) => {
+  let date = str ? moment(str) : moment();
+  return date.format('YYYY-MM-DD HH:mm:ss');
+};
+export const getSamlpeDate = (str) => {
+  let date = str ? moment(str) : moment();
+  return date.format('YYYY-MM-DD');
+};
+export const isToday = (date) => {
+  return moment().isSame(date, 'day');
+};
+export const getDateDiff = (prevDate, nextDate) => {
+  return new Date(nextDate) - new Date(prevDate);
+};
+
+const helper = {
+  getShortDiff,
+  getShortDate,
+  getMsgDate,
+  getFullDate,
+  getSamlpeDate,
+  isToday,
+  getDateDiff,
 };
 
 export default helper;
