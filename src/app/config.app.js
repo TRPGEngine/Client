74d22244ff9e9013e00c4568e57566bf1cd5ec27
moonrailgun <moonrailgun@gmail.com>
2019-05-30 09:11:45 +0800

    :hammer: 将图片资源复制到app文件夹解决无法正常获取资源文件的问题

--- src/app/config.app.js
+++ src/app/config.app.js
@@ -5,16 +5,16 @@ const out = {
   defaultImg: {
     user: '', // 让系统系统生成
     group: require('../assets/img/gugugu1.png'),
-    trpgsystem: require('../assets/img/system_notice.png'),
+    trpgsystem: require('./assets/img/system_notice.png'),
     actor: null,
     file: {
-      default: require('../assets/img/file/default.png'),
-      pdf: require('../assets/img/file/pdf.png'),
-      excel: require('../assets/img/file/excel.png'),
-      ppt: require('../assets/img/file/ppt.png'),
-      word: require('../assets/img/file/word.png'),
-      txt: require('../assets/img/file/txt.png'),
-      pic: require('../assets/img/file/pic.png'),
+      default: require('./assets/img/file/default.png'),
+      pdf: require('./assets/img/file/pdf.png'),
+      excel: require('./assets/img/file/excel.png'),
+      ppt: require('./assets/img/file/ppt.png'),
+      word: require('./assets/img/file/word.png'),
+      txt: require('./assets/img/file/txt.png'),
+      pic: require('./assets/img/file/pic.png'),
     },
   },
   file: {
