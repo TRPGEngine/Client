9eac25d7c7b714ae1660573771cb4b120a2eef48
moonrailgun <moonrailgun@gmail.com>
2019-07-17 15:46:43 +0800

    :hammer: [web] bump to ts

--- src/web/components/TLoadable.ts
+++ src/web/components/TLoadable.ts
@@ -1,8 +1,12 @@
 import Loadable from 'react-loadable';
 import LoadingSpinner from './LoadingSpinner';
+import { ComponentType } from 'react';
 
-// 用法: Loadable(() => import('xxxxxx'))
-export const TLoadable = (loader) => {
+/**
+ * 用法: Loadable(() => import('xxxxxx'))
+ * @param loader 需要懒加载的组件
+ */
+export const TLoadable = (loader: any) => {
   return Loadable({
     loader,
     loading: LoadingSpinner,
