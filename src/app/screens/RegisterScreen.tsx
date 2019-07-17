721fa9b26eaa055e3bdab6afa715ec3f5217acb4
moonrailgun <moonrailgun@gmail.com>
2019-07-17 22:22:30 +0800

    :bug: [app] 修复注册成功后点击弹出模态框无法关闭的bug

--- src/app/screens/RegisterScreen.tsx
+++ src/app/screens/RegisterScreen.tsx
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import sb from 'react-native-style-block';
-import { TButton, TFormGroup, TLoading } from '../components/TComponent';
-import { showLoading, showAlert } from '../../redux/actions/ui';
+import { TButton, TFormGroup } from '../components/TComponent';
+import { showLoading, showAlert, hideAlert } from '../../redux/actions/ui';
 import { register } from '../../redux/actions/user';
 
 interface Props {
@@ -71,6 +71,7 @@ class RegisterScreen extends React.Component<Props, State> {
             showAlert({
               content: '注册成功',
               onConfirm: () => {
+                this.props.dispatch(hideAlert());
                 this.props.dispatch(NavigationActions.back());
               },
             })
