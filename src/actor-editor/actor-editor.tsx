0680605e1c2d1c7c54c2bcb1a376b488af5779de
moonrailgun <moonrailgun@gmail.com>
2019-07-27 00:24:31 +0800

    :star: [web] 增加独立应用人物模板编辑器

---
+++ src/actor-editor/actor-editor.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
+import SplitPane from 'react-split-pane';
+import MonacoEditor, { EditorDidMount } from 'react-monaco-editor';
+import './split-pane.css';
+import { editor, IPosition, KeyMod, KeyCode } from 'monaco-editor';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: white;
+`;
+
+const Block = styled.div<{ label: string }>`
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+
+  &:before {
+    content: '${(props) => props.label || ''}';
+    padding: 0.5rem 2rem;
+    background-color: #333;
+    display: block;
+    color: white;
+    font-size: 16px;
+    border-bottom: 1px solid #111;
+  }
+`;
+
+const editorOptions = {
+  selectOnLineNumbers: true,
+  automaticLayout: true,
+};
+
+const initCode = `<?xml version="1.0" encoding="utf-8" ?>
+<Template>
+  <!-- 在此处输入你的布局 -->
+
+</Template>
+`;
+const initCodePos: IPosition = {
+  lineNumber: 3,
+  column: 21,
+};
+
+const ActorEditor = () => {
+  const [code, setCode] = useState<string>(initCode);
+  const editorRef = useRef<editor.IStandaloneCodeEditor>();
+
+  const onEditorDidMount: EditorDidMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+    editor.setPosition(initCodePos);
+    console.log(KeyMod.CtrlCmd, KeyCode.KEY_S, KeyMod.CtrlCmd | KeyCode.KEY_S);
+    editor.addCommand(KeyMod.CtrlCmd | KeyCode.KEY_S, () => {
+      console.log('TODO: save');
+    });
+  };
+
+  return (
+    <Container>
+      <SplitPane split="vertical" defaultSize="50%">
+        <SplitPane split="horizontal" defaultSize="60%">
+          <Block label="布局">
+            <MonacoEditor
+              width="100%"
+              height="100%"
+              language="xml"
+              theme="vs-dark"
+              value={code}
+              options={editorOptions}
+              onChange={(newValue) => setCode(newValue)}
+              editorDidMount={onEditorDidMount}
+            />
+          </Block>
+          <div></div>
+        </SplitPane>
+        <div>asad</div>
+      </SplitPane>
+    </Container>
+  );
+};
+
+export default ActorEditor;
---
+++ src/actor-editor/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import ActorEditor from './actor-editor';
+
+ReactDom.render(<ActorEditor />, document.querySelector('#app'));
