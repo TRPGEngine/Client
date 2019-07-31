4fdeaa29623a12e8c55c9a3a8be1372a54825750
moonrailgun <moonrailgun@gmail.com>
2019-07-31 09:32:00 +0800

    :star: [app] add react-native-config

--- android/app/src/main/java/com/moonrailgun/trpg/MainApplication.java
+++ android/app/src/main/java/com/moonrailgun/trpg/MainApplication.java
@@ -3,6 +3,7 @@ package com.moonrailgun.trpg;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+        new ReactNativeConfigPackage(),
         new PickerPackage(),
         new FastImageViewPackage(),
         new RNCWebViewPackage(),
