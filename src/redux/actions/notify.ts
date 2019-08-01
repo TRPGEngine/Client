006293708f305ef7c19e63680ee1bdc812dd06c3
moonrailgun <moonrailgun@gmail.com>
2019-08-01 19:05:22 +0800

    :hammer: [redux] add notify bind info event

--- src/redux/actions/notify.ts
+++ src/redux/actions/notify.ts
@@ -10,12 +10,18 @@ interface NotifyInfo {
   platform: PlatformType;
 }
 
+const notifyEventNameMap = {
+  jpush: 'notify::bindNotifyInfo',
+  upush: 'notify::bindUPushNotifyInfo',
+};
+
 export const bindNotifyInfo = async (info: NotifyInfo) => {
   const platform = info.platform;
   const storageKey = 'hasBindNotifyInfo_' + platform;
   const hasBindNotifyInfo = await rnStorage.get(storageKey);
   if (!hasBindNotifyInfo) {
-    api.emit('notify::bindNotifyInfo', { info }, (data) => {
+    const eventName = notifyEventNameMap[platform];
+    api.emit(eventName, { info }, (data) => {
       if (data.result) {
         rnStorage.save(storageKey, true);
       }
