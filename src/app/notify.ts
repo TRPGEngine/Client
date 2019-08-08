79351864c540ae88ccc2aac2d2f15455e48c4991
moonrailgun <moonrailgun@gmail.com>
2019-08-08 09:24:14 +0800

    :hammer: [app] remove jpush in js

--- src/app/notify.ts
+++ src/app/notify.ts
@@ -4,58 +4,24 @@ import { bindNotifyInfo } from '../redux/actions/notify';
 import rnStorage from '../api/rn-storage.api';
 import { umPush, Code } from './native/push-utils';
 
-enum PushPlatformType {
-  JPush, // 极光推送
-  UPush, // 友盟推送
-}
-const PushPlatform: PushPlatformType = PushPlatformType.UPush; // 此处配置推送的平台
-
-/**
- * 初始化通知模块
- */
-export function initNotify() {
-  if (PushPlatform === PushPlatformType.JPush) {
-    // JPushModule.initPush();
-  } else if (PushPlatform === PushPlatformType.UPush) {
-    umPush.register((code, ret) => {
-      if (code === Code.ERROR) {
-        console.error('通知服务初始化失败', ret);
-      }
-    });
-  }
-}
-
 // alias为用户的uuid
-export async function setAlias(alias: string, platform: 'jpush' | 'upush') {
+export async function setAlias(alias: string) {
   if (!alias) {
     console.warn('setAlias failed: alias is required', alias);
     return;
   }
 
-  if (platform === 'jpush') {
-    const hasSetJPushAlias = await rnStorage.get('hasSetJPushAlias');
-    if (!hasSetJPushAlias || hasSetJPushAlias !== alias) {
-      // 如果没有设置别名或设置的别名不为当前用户，则重新设置
-      // JPushModule.setAlias(alias, (success) => {
-      //   console.log('[JPush]', 'set alias success', success);
-      //   rnStorage.save('hasSetJPushAlias', alias);
-      // });
-    }
-  }
-
-  if (platform === 'upush') {
-    const hasSetUPushAlias = await rnStorage.get('hasSetUPushAlias');
-    if (!hasSetUPushAlias || hasSetUPushAlias !== alias) {
-      // 如果没有设置别名或设置的别名不为当前用户，则重新设置
-      umPush.addAlias(alias, 'trpg_user', (success) => {
-        if (success === 200) {
-          console.log('[UPush]', 'set alias success', success);
-          rnStorage.save('hasSetJPushAlias', alias);
-        } else {
-          console.error('[UPush]', 'set alias error');
-        }
-      });
-    }
+  const hasSetUPushAlias = await rnStorage.get('hasSetUPushAlias');
+  if (!hasSetUPushAlias || hasSetUPushAlias !== alias) {
+    // 如果没有设置别名或设置的别名不为当前用户，则重新设置
+    umPush.addAlias(alias, 'trpg_user', (success) => {
+      if (success === Code.SUCCESS) {
+        console.log('[UPush]', 'set alias success', success);
+        rnStorage.save('hasSetJPushAlias', alias);
+      } else {
+        console.error('[UPush]', 'set alias error');
+      }
+    });
   }
 }
 
@@ -64,23 +30,11 @@ export async function setAlias(alias: string, platform: 'jpush' | 'upush') {
  * @param userUUID 用户UUID
  */
 export function bindInfo(userUUID: string) {
-  if (PushPlatform === PushPlatformType.JPush) {
-    // 极光推送
-    // JPushModule.getRegistrationID((registrationID) => {
-    //   bindNotifyInfo({
-    //     userUUID,
-    //     registrationID: String(registrationID),
-    //     platform: 'jpush',
-    //   });
-    //   setAlias(userUUID, 'jpush');
-    // });
-  } else if (PushPlatform === PushPlatformType.UPush) {
-    // 友盟推送
-    umPush.getRegistrationId((registrationID) => {
-      bindNotifyInfo({ userUUID, registrationID, platform: 'upush' });
-      setAlias(userUUID, 'upush');
-    });
-  }
+  // 友盟推送
+  umPush.getRegistrationId((registrationID) => {
+    bindNotifyInfo({ userUUID, registrationID, platform: 'upush' });
+    setAlias(userUUID);
+  });
 }
 
 // 监测app状态
