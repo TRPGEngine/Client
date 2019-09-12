d9827781f0dabfd4e71c6f31eef94ad8de322b30
moonrailgun <moonrailgun@gmail.com>
2019-09-13 00:29:28 +0800

    :fire: [app] app jest test

--- src/app/jest.config.js
+++ src/app/jest.config.js
@@ -3,25 +3,24 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('../../tsconfig');
 
-module.exports = Object.assign({}, baseConfig, {
+module.exports = {
   ...tsjPreset,
   preset: 'react-native',
-  transformIgnorePatterns: [],
   transform: {
     ...tsjPreset.transform,
     '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-    '^.+\\.test.(ts|tsx)$': 'ts-jest',
+    '\\.(ts|tsx)$': 'ts-jest',
   },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/../../',
   }),
-  transformIgnorePatterns: [
-    'node_modules/(?!react-native|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|tcomb-form-native|react-navigation-stack|react-navigation)',
-  ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
+  rootDir: '.',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.json',
+      babelConfig: true,
     },
   },
-});
+  cacheDirectory: '.jest/cache',
+};
