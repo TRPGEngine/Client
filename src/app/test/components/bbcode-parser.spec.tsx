cf3832c86a7a0adbc429762bb762343f784e5bd5
moonrailgun <moonrailgun@gmail.com>
2019-09-13 15:46:33 +0800

    :star: [app] add bbcode testcase

--- src/app/test/components/bbcode-parser.spec.tsx
+++ src/app/test/components/bbcode-parser.spec.tsx
@@ -2,10 +2,31 @@ import React, { Fragment } from 'react';
 import bbcodeParser from '../../src/utils/bbcode-parser';
 import renderer from 'react-test-renderer';
 
+const cases = [
+  {
+    title: 'plain text',
+    bbcode: 'TestString',
+  },
+  {
+    title: 'plain text with space',
+    bbcode: 'Test String',
+  },
+  {
+    title: 'pure image',
+    bbcode: '[img]http://exampleimg.com[/img]',
+  },
+  {
+    title: 'mix image and text',
+    bbcode: '测试文字1[img]http://exampleimg.com[/img]测试文字2',
+  },
+];
+
 describe('bbcode-parser', () => {
-  it('plain text', () => {
-    const Component = bbcodeParser.parse('TestString');
-    const msg = renderer.create(<Fragment>{Component}</Fragment>).toJSON();
-    expect(msg).toMatchSnapshot();
-  });
+  for (const item of cases) {
+    it(item.title, () => {
+      const Component = bbcodeParser.parse(item.bbcode);
+      const result = renderer.create(<Fragment>{Component}</Fragment>).toJSON();
+      expect(result).toMatchSnapshot();
+    });
+  }
 });
