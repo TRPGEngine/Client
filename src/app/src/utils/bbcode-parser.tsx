15a6e6c22b867403afb1185a0863dd350235bb7f
moonrailgun <moonrailgun@gmail.com>
2019-09-16 16:57:07 +0800

    :hammer: [app] 调整解释器，多个Text组件将会合并

--- src/app/src/utils/bbcode-parser.tsx
+++ src/app/src/utils/bbcode-parser.tsx
@@ -4,6 +4,7 @@ import { parse } from '@bbob/parser';
 import ImageTag from './bbcode/ImageTag';
 import { AstNode } from './bbcode/types';
 import _has from 'lodash/has';
+import _last from 'lodash/last';
 
 const tagMap = {
   img: ImageTag,
@@ -23,18 +24,29 @@ class BBCodeParser {
   parse(input: string): ReactElement[] {
     const ast = parse(input, this.options) as AstNode[];
 
-    return ast.map<ReactElement>((node, index) => {
-      if (typeof node === 'string') {
-        return <Text key={index}>{node}</Text>;
-      }
+    return ast
+      .reduce<AstNode[]>((prev, curr) => {
+        if (typeof curr === 'string' && typeof _last(prev) === 'string') {
+          // 合并字符串, 使其渲染时能公用一个Text组件
+          prev[prev.length - 1] += curr;
+        } else {
+          prev.push(curr);
+        }
 
-      if (typeof node === 'object' && _has(tagMap, node.tag)) {
-        const Component = tagMap[node.tag];
-        return <Component key={index} node={node} />;
-      }
+        return prev;
+      }, [])
+      .map<ReactElement>((node, index) => {
+        if (typeof node === 'string') {
+          return <Text key={index}>{node}</Text>;
+        }
 
-      return null;
-    });
+        if (typeof node === 'object' && _has(tagMap, node.tag)) {
+          const Component = tagMap[node.tag];
+          return <Component key={index} node={node} />;
+        }
+
+        return null;
+      });
   }
 }
 
