ad9a64b057474826b5f54a1d9874058a0b969b5b
moonrailgun <moonrailgun@gmail.com>
2019-10-05 15:20:05 +0800

    :star: [misc] add styled theme

---
+++ src/shared/utils/theme.ts
@@ -0,0 +1,36 @@
+// name from http://chir.ag/projects/name-that-color/
+
+const styledTheme = {
+  color: {
+    'seashell-peach': '#FFF4EB',
+    whisper: '#FAFAFC',
+    soapstone: '#FFFBF8',
+    'white-linen': '#FBF5F1',
+    'wild-sand': '#F5F5F5',
+    'pearl-bush': '#F1EBE6',
+    saltpan: '#F9FCFA',
+    'bon-jour': '#DDDCDD',
+    silver: '#CCCCCC',
+    'french-gray': '#C7C7CC',
+    'silver-sand': '#BDBEBF',
+    'dusty-gray': '#999999',
+    gray: '#878787',
+    'dove-gray': '#666666',
+    tundora: '#4a4a4a',
+    'mine-shaft': '#333333',
+    'cod-gray': '#222222',
+    'jungle-mist': '#BACED9',
+    'tobacco-brown': '#705949',
+    'copper-canyon': '#7E3F12',
+    'spicy-mix': '#8C6244',
+    'atomic-tangerine': '#FFA160',
+    tacao: '#EDAB7C',
+    'pine-cone': '#726155',
+    tan: '#D0A282',
+    downy: '#65CAB0',
+    gold: '#FFD700',
+    'alizarin-crimson': '#EC2121',
+  },
+};
+
+export default styledTheme;
--- src/web/theme.ts
+++
@@ -1,9 +0,0 @@
-import { Store } from 'redux';
-
-/**
- * 返回一个theme类用于styled-component
- * @param store redux store
- */
-export const getTheme = (store: Store) => {
-  return {};
-};
