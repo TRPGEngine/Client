d33948d8890eed33651d8e1df7a9d5985ac2b828
moonrailgun <moonrailgun@gmail.com>
2019-10-16 18:06:46 +0800

    :star: [app] 创建团UI 使用formik管理表单数据

--- src/app/src/screens/CreateGroupScreen.tsx
+++ src/app/src/screens/CreateGroupScreen.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { List, InputItem, TextareaItem } from '@ant-design/react-native';
+import { Formik } from 'formik';
+import { TButton } from '../components/TComponent';
 
 class CreateGroupScreen extends React.Component {
   render() {
-    return <Text>CreateGroupScreen</Text>;
+    return (
+      <Formik
+        initialValues={{ groupName: '', groupSubName: '', groupDesc: '' }}
+        onSubmit={(values) => alert(JSON.stringify(values))}
+      >
+        {({ values, handleChange, handleSubmit }) => (
+          <List>
+            <InputItem
+              clear
+              value={values.groupName}
+              onChange={handleChange('groupName')}
+            >
+              团名
+            </InputItem>
+            <InputItem
+              clear
+              value={values.groupSubName}
+              onChange={handleChange('groupSubName')}
+            >
+              团副名
+            </InputItem>
+            <TextareaItem
+              rows={4}
+              value={values.groupDesc}
+              onChange={handleChange('groupDesc')}
+            />
+            <List.Item>
+              <TButton onPress={handleSubmit}>创建</TButton>
+            </List.Item>
+          </List>
+        )}
+      </Formik>
+    );
   }
 }
 
