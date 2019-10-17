8762e262d8e2846e6e2e2046af294b5cb839422d
moonrailgun <moonrailgun@gmail.com>
2019-10-17 13:47:24 +0800

    :hammer: [all] 增加部分redux action 类型

--- src/shared/redux/types/redux.ts
+++ src/shared/redux/types/redux.ts
@@ -1,7 +1,21 @@
 import { AnyAction, Action } from 'redux';
-import { ThunkDispatch, ThunkAction } from 'redux-thunk';
+
+// fork from redux-thunk declaration
+interface ThunkDispatch<S, E, A extends Action> {
+  <T extends A>(action: T): T;
+  <R>(asyncAction: TRPGAction): R;
+}
+
+type ThunkAction<R, S, E, A extends Action> = (
+  dispatch: ThunkDispatch<S, E, A>,
+  getState: () => S,
+  extraArgument: E
+) => R;
 
 // 用于组件声明
 export interface TRPGDispatchProp<A extends Action = AnyAction> {
   dispatch: ThunkDispatch<any, any, A>;
 }
+
+// 用于action声明
+export type TRPGAction = ThunkAction<any, any, any, AnyAction> | AnyAction;
