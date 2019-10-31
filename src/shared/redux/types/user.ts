5c90c68cd59ffade1c615e49077dc4c01b70aa44
moonrailgun <moonrailgun@gmail.com>
2019-10-31 10:08:27 +0800

    :star: [all] 补充了部分user与group类型

--- src/shared/redux/types/user.ts
+++ src/shared/redux/types/user.ts
@@ -1,10 +1,41 @@
 import { Record, List } from 'immutable';
 
+export type UserAlignment =
+  | 'LG'
+  | 'NG'
+  | 'CG'
+  | 'LN'
+  | 'TN'
+  | 'CN'
+  | 'LE'
+  | 'NE'
+  | 'CE';
+
+export type UserGender = '男' | '女' | '其他' | '保密';
+
+export interface UserInfo {
+  id: number;
+  uuid: string;
+  username: string;
+  nickname: string;
+  name: string;
+  avatar: string;
+  last_login: string;
+  last_ip: string;
+  token?: string;
+  app_token?: string;
+  sex: UserGender;
+  sign: string;
+  alignment: UserAlignment;
+}
+
+export type UserStateInfo = Record<UserInfo>;
+
 export type UserState = Record<{
   isTryLogin: boolean;
   isLogin: boolean;
-  info: any;
-  friendList: List<any>;
+  info: UserStateInfo;
+  friendList: List<string>;
   friendInvite: List<any>;
   friendRequests: List<any>;
   isFindingUser: boolean;
