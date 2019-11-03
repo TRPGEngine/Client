3c13a449d76fb6359e7363e6484b9f3c3260ee95
moonrailgun <moonrailgun@gmail.com>
2019-11-03 16:53:57 +0800

    :star: [portal] 增加全局事件并增加actor editor的数据获取方法

--- src/portal/App.tsx
+++ src/portal/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from '@portal/sso/Login';
 import ActorEditor from './actor/editor';
+import './utils/event';
 
 class App extends React.Component {
   render() {
---
+++ src/portal/types/events.ts
@@ -0,0 +1,1 @@
+export type EventTypes = 'actor::actordata';
