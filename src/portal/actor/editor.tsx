afe4aeb238d3c9d20bda43f52370bcf55a5fa6cb
moonrailgun <moonrailgun@gmail.com>
2019-11-10 14:31:57 +0800

    :star: [layout] 增加无限恐怖人物卡

--- src/portal/actor/editor.tsx
+++ src/portal/actor/editor.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import XMLBuilder, { DataType } from '@src/shared/layout/XMLBuilder';
 import { on, off, postMessage } from '../utils/event';
-const exampleXml = require('@shared/layout/example/coc7-layout.xml').default;
+const exampleXml = require('@shared/layout/example/wuxian-layout.xml').default;
 
 class ActorEditor extends React.Component {
   actorData: DataType = {};
@@ -12,10 +12,12 @@ class ActorEditor extends React.Component {
 
   componentDidMount() {
     on('actor::getActorData', this.getActorData);
+    (window as any).getActorData = () => this.actorData;
   }
 
   componentWillMount() {
     off('actor::getActorData', this.getActorData);
+    (window as any).getActorData = null;
   }
 
   render() {
