70b394461c63937f951ec42c7031f2615ad4fa26
moonrailgun <moonrailgun@gmail.com>
2019-11-19 16:41:38 +0800

    :star: [portal] 增加路由标题

--- src/portal/App.tsx
+++ src/portal/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route, RouteProps } from 'react-router';
 import Login from '@portal/sso/Login';
 import ActorList from '@portal/actor/list';
 import ActorSelectTemplate from './actor/select-template';
@@ -10,35 +10,57 @@ import history from './history';
 import ActorDetail from './actor/detail';
 import ActorEdit from './actor/edit';
 
+interface TitleRouteProps extends RouteProps {
+  title: string;
+}
+const TitleRoute = (route: TitleRouteProps) => {
+  return (
+    <Route
+      path={route.path}
+      render={(props) => {
+        document.title = route.title || 'TRPG Portal';
+
+        const Component = route.component;
+
+        return <Component {...props}>{route.children}</Component>;
+      }}
+    />
+  );
+};
+
 class App extends React.Component {
   render() {
     return (
       <Router history={history}>
         <Switch>
-          <Route name="login" path="/sso/login" component={Login} />
-          <Route name="actor-list" path="/actor/list" component={ActorList} />
-          <Route
-            name="actor-select-template"
+          <TitleRoute title="登录" path="/sso/login" component={Login} />
+          <TitleRoute
+            title="人物列表"
+            path="/actor/list"
+            component={ActorList}
+          />
+          <TitleRoute
+            title="选择模板"
             path="/actor/create/select-template"
             component={ActorSelectTemplate}
           />
-          <Route
-            name="actor-detail"
+          <TitleRoute
+            title="人物详情"
             path="/actor/detail/:actorUUID"
             component={ActorDetail}
           />
-          <Route
-            name="actor-edit"
+          <TitleRoute
+            title="人物编辑"
             path="/actor/edit/:actorUUID"
             component={ActorEdit}
           />
-          <Route
-            name="actor-create"
+          <TitleRoute
+            title="创建人物"
             path="/actor/create/template/:templateUUID"
             component={ActorCreate}
           />
-          <Route
-            name="actor-editor"
+          <TitleRoute
+            title="人物编辑器"
             path="/actor/editor"
             component={ActorEditor}
           />
