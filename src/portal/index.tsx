bbf783f653718db0f695760d6f86dca122bbf416
moonrailgun <moonrailgun@gmail.com>
2019-11-30 13:29:13 +0800

    :hammer: [portal] 默认背景颜色置白

--- src/portal/index.tsx
+++ src/portal/index.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import '@web/assets/css/iconfont.css';
+import './index.css';
 
 import styledTheme from '@src/shared/utils/theme';
 
