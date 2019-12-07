c5798b5be0cfb933a8f74fa0c764465c6e05affc
moonrailgun <moonrailgun@gmail.com>
2019-12-07 12:51:03 +0800

    test: jest静态资源文件处理

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,18 @@ const run = _cloneDeep(regeneratorRuntime);
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>/',
+    }),
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
   rootDir: '.',
   roots: ['<rootDir>/test/', '<rootDir>/src/web/test/'],
+  transform: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test/fileTransformer.js',
+  },
   transformIgnorePatterns: ['/node_modules/'],
   setupFilesAfterEnv: [
     require.resolve('./test/global.ts'), // 注册全局
