912695f46ef97c9ffa8b28c2c27d870ad8b1c094
moonrailgun <moonrailgun@gmail.com>
2019-12-07 12:58:26 +0800

    test: add enzyme

--- test/global.ts
+++ test/global.ts
@@ -1,3 +1,8 @@
 // 为使react-native-storage能够正常运行需要有一个全局的regeneratorRuntime
 // 网页端由babel-runtime提供， 而jest使用ts-node解释js文件。而不是使用babel。因此要手动全局引入
 import 'regenerator-runtime/runtime';
+
+// enzyme react 支持
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
