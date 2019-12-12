72333ca156935b29934624759ce0911c3de9b3dc
moonrailgun <moonrailgun@gmail.com>
2019-12-12 09:26:35 +0800

    refactor: 抽象web的测试用户校验，并移除测试环境下api的信息打印
    在cli中打印socket api太长了影响感官

--- src/web/components/DevContainer.tsx
+++ src/web/components/DevContainer.tsx
@@ -1,13 +1,5 @@
 import React, { Fragment } from 'react';
-import memoizeOne from 'memoize-one';
-
-/**
- * 获取是否为测试用户
- * 方法是在localStorage上设置 trpg_test: true
- */
-const checkIsTestUser = memoizeOne(() => {
-  return localStorage.getItem('trpg_test') === 'true';
-});
+import { checkIsTestUser } from '@web/utils/debug-helper';
 
 const DevContainer: React.FC<{}> = React.memo((props) => {
   return checkIsTestUser() ? <Fragment>{props.children}</Fragment> : null;
