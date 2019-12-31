d9ca7b143fa72cef4e262cebd6b1b50c12d5099f
moonrailgun <moonrailgun@gmail.com>
2020-01-01 00:29:34 +0800

    refactor: upgrade utils/timer and remove timernumber when clear it

--- src/shared/utils/timer.ts
+++ src/shared/utils/timer.ts
@@ -1,13 +1,22 @@
 const _timerMapping = {};
 
+function removeTimerMap(key: string) {
+  clearTimeout(_timerMapping[key]);
+  _timerMapping[key] = undefined;
+}
+
 // 根据key，如果该方法在该任务触发前再次被调用，则取消之前的任务生成一个新的任务，即续期效果
-export function renewableDelayTimer(key, fn, millisecond) {
+export function renewableDelayTimer(
+  key: string,
+  fn: Function,
+  millisecond: number
+) {
   if (_timerMapping[key]) {
     // 如果之前已经有该任务了, 则取消
-    clearTimeout(_timerMapping[key]);
+    removeTimerMap(key);
   }
   _timerMapping[key] = setTimeout(function() {
     fn();
-    clearTimeout(_timerMapping[key]);
+    removeTimerMap(key);
   }, millisecond);
 }
