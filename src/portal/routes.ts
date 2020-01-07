40973b9ea33b69048a1e86d048c92ddf2843486b
moonrailgun <moonrailgun@gmail.com>
2020-01-07 16:41:40 +0800

    refactor(portal): 迁移route配置到独立配置

---
+++ src/portal/routes.ts
@@ -0,0 +1,71 @@
+import TLoadable from '@web/components/TLoadable';
+import { ComponentType } from 'react';
+
+interface RouteConfig {
+  title: string;
+  path: string;
+  component: ComponentType<any>;
+}
+
+export const routes: RouteConfig[] = [
+  {
+    title: '登录',
+    path: '/sso/login',
+    component: TLoadable(() => import('@portal/routes/sso/Login')),
+  },
+  {
+    title: '人物列表',
+    path: '/actor/list',
+    component: TLoadable(() => import('@portal/routes/actor/list')),
+  },
+  {
+    title: '选择模板',
+    path: '/actor/create/select-template',
+    component: TLoadable(() => import('@portal/routes/actor/select-template')),
+  },
+  {
+    title: '人物详情',
+    path: '/actor/detail/:actorUUID',
+    component: TLoadable(() => import('@portal/routes/actor/detail')),
+  },
+  {
+    title: '人物编辑',
+    path: '/actor/edit/:actorUUID',
+    component: TLoadable(() => import('@portal/routes/actor/edit')),
+  },
+  {
+    title: '创建人物',
+    path: '/actor/create/template/:templateUUID',
+    component: TLoadable(() => import('@portal/routes/actor/create')),
+  },
+  {
+    title: '人物编辑器',
+    path: '/actor/editor',
+    component: TLoadable(() => import('@portal/routes/actor/editor')),
+  },
+  {
+    title: '团人物列表',
+    path: '/group/:groupUUID/actor/list',
+    component: TLoadable(() => import('@portal/routes/actor/list')),
+  },
+  {
+    title: '团人物详情',
+    path: '/group/:groupUUID/actor/:groupActorUUID/detail',
+    component: TLoadable(() => import('@portal/routes/group/actor/detail')),
+  },
+  {
+    title: '团人物编辑',
+    path: '/group/:groupUUID/actor/:groupActorUUID/edit',
+    component: TLoadable(() => import('@portal/routes/group/actor/edit')),
+  },
+  {
+    title: '创建笔记',
+    path: '/note/create',
+    component: TLoadable(() => import('@portal/routes/note/create')),
+  },
+  {
+    title: '下载App',
+    path: '/deploy',
+    component: TLoadable(() => import('@portal/routes/deploy')),
+  },
+];
