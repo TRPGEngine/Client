2ba433f65f1c2e762f2edc59b9d3312f7c089958
moonrailgun <moonrailgun@gmail.com>
2020-01-16 10:41:48 +0800

    refactor(web): 增加ImageView对多图片切换的支持

--- src/web/components/ImageViewer.tsx
+++ src/web/components/ImageViewer.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import Lightbox from 'react-image-lightbox';
+import _isEmpty from 'lodash/isEmpty';
+import _findLastIndex from 'lodash/findLastIndex';
 
 import './ImageViewer.scss';
 
 interface Props {
   originImageUrl: string;
+  allImageUrls?: string[];
 }
 class ImageViewer extends React.PureComponent<Props> {
+  static defaultProps = {
+    allImageUrls: [],
+  };
+
   state = {
     isOpen: false,
+    currentIndex: -1,
   };
+
+  constructor(props: Props) {
+    super(props);
+    if (!_isEmpty(props.allImageUrls)) {
+      // 先简单一点，只找到最后一张图片地址一样的
+      this.state.currentIndex = _findLastIndex(
+        props.allImageUrls,
+        (url) => url === props.originImageUrl
+      );
+    }
+  }
 
   handleClick() {
     if (this.props.originImageUrl) {
@@ -17,14 +36,48 @@ class ImageViewer extends React.PureComponent<Props> {
     }
   }
 
+  /**
+   * 获取当前图片的Url
+   */
+  getImageUrlByOffset(offset: number): string {
+    const { originImageUrl, allImageUrls } = this.props;
+
+    if (this.state.currentIndex === -1) {
+      return offset === 0 ? originImageUrl : undefined;
+    }
+
+    return allImageUrls[this.state.currentIndex + offset] ?? undefined;
+  }
+
+  handleCloseRequest = () => {
+    this.setState({ isOpen: false });
+  };
+
+  handleMovePrevRequest = () => {
+    this.setState({
+      currentIndex: this.state.currentIndex - 1,
+    });
+  };
+
+  handleMoveNextRequest = () => {
+    this.setState({
+      currentIndex: this.state.currentIndex + 1,
+    });
+  };
+
   render() {
     return (
       <div className="image-viewer" onClick={() => this.handleClick()}>
         {this.props.children}
         {this.props.originImageUrl && this.state.isOpen && (
           <Lightbox
-            mainSrc={this.props.originImageUrl}
-            onCloseRequest={() => this.setState({ isOpen: false })}
+            mainSrc={this.getImageUrlByOffset(0)}
+            prevSrc={this.getImageUrlByOffset(-1)}
+            nextSrc={this.getImageUrlByOffset(+1)}
+            imageLoadErrorMessage="图片加载失败"
+            onCloseRequest={this.handleCloseRequest}
+            onMovePrevRequest={this.handleMovePrevRequest}
+            onMoveNextRequest={this.handleMoveNextRequest}
           />
         )}
       </div>
