6b7cec1be99bfcd5dae2f0074fdc3181c8730263
moonrailgun <moonrailgun@gmail.com>
2020-01-21 16:47:17 +0800

    refactor: useWebsiteInfo修改回使用bbcode解释器来查找url地址的方法
    不直接通过正则获取url是因为可能会获取到图片url之类的其他地图

--- src/shared/hooks/useWebsiteInfo.ts
+++ src/shared/hooks/useWebsiteInfo.ts
@@ -3,7 +3,9 @@ import _isObject from 'lodash/isObject';
 import _isNil from 'lodash/isNil';
 import _isEmpty from 'lodash/isEmpty';
 import request from '@shared/utils/request';
-import { getUrls } from '@shared/utils/string-helper';
+import { preProcessText } from '@shared/components/bbcode';
+import bbcodeParser from '@shared/components/bbcode/parser';
+import { AstNodeObj } from '@shared/components/bbcode/type';
 
 interface WebsiteInfo {
   title: string;
@@ -20,9 +22,21 @@ export const useWebsiteInfo = (rawMessage: string) => {
     url: '',
   });
   const messageUrl = useMemo(() => {
-    const urls = getUrls(rawMessage);
-    if (urls.length > 0) {
-      return urls[0]; // 只获取第一个
+    // 此处使用bbcode解释器作为获取Url的操作。
+    // 不直接通过正则获取url是因为可能会获取到图片url之类的其他地图
+    const message = preProcessText(rawMessage);
+
+    if (message.indexOf('[url]') >= 0) {
+      const ast = bbcodeParser.parse(message);
+      const urlTag = ast.find(
+        (node) => _isObject(node) && node.tag === 'url'
+      ) as AstNodeObj; // 只取第一个
+
+      if (_isNil(urlTag)) {
+        return '';
+      }
+
+      return urlTag.content.join('');
     }
 
     return '';
