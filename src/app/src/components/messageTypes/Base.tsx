4f3c4fc0e1fce5e29edaf4499937dfe6fe4e2148
moonrailgun <moonrailgun@gmail.com>
2020-02-02 00:08:47 +0800

    perf(app): 优化对话界面消息列表的渲染触发。降低重绘

--- src/app/src/components/messageTypes/Base.tsx
+++ src/app/src/components/messageTypes/Base.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import sb from 'react-native-style-block';
 import { TAvatar } from '../TComponent';
-import dateHelper from '../../../../shared/utils/date-helper';
-import config from '../../../../shared/project.config';
+import dateHelper from '@shared/utils/date-helper';
 import { MessageProps } from '@shared/components/MessageHandler';
 import _get from 'lodash/get';
 import { getAbsolutePath } from '@shared/utils/file-helper';
 import { TipMessage } from '../TipMessage';
 
-class Base<P extends MessageProps = MessageProps> extends React.Component<P> {
+class Base<P extends MessageProps = MessageProps> extends React.PureComponent<
+  P
+> {
   static defaultProps = {
     type: 'normal',
     me: false,
