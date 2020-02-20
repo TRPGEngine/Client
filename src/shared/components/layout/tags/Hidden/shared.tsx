4f8586e02dd2854e86d28d64d2e1af8678892095
moonrailgun <moonrailgun@gmail.com>
2020-02-20 16:53:18 +0800

    feat(layout): 增加Hidden组件if参数的支持

--- src/shared/components/layout/tags/Hidden/shared.tsx
+++ src/shared/components/layout/tags/Hidden/shared.tsx
@@ -1,16 +1,22 @@
-import React, { useContext, Fragment } from 'react';
+import React, { useContext, Fragment, useMemo } from 'react';
 import { TagComponent } from '../type';
 import { LayoutStateContext } from '../../context/LayoutStateContext';
 import { useLayoutChildren } from '../../hooks/useLayoutChildren';
 
 interface TagProps {
   in: 'detail' | 'edit';
+  if: any;
 }
 export const TagHiddenShared: TagComponent<TagProps> = React.memo((props) => {
   const { layoutType } = useContext(LayoutStateContext);
   const children = useLayoutChildren(props);
 
-  if (layoutType === props.in) {
+  const isHidden = useMemo(() => layoutType === props.in || props.if === true, [
+    props.in,
+    props.if,
+  ]);
+
+  if (isHidden) {
     return null;
   } else {
     return <Fragment>{children}</Fragment>;
