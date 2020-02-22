d8748180b2fafd04b7ccd6c78839c027e28ee272
moonrailgun <moonrailgun@gmail.com>
2020-02-22 14:09:05 +0800

    perf(web): 优化代码

--- src/web/components/Avatar.tsx
+++ src/web/components/Avatar.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Avatar as AntdAvatar } from 'antd';
 import _head from 'lodash/head';
+import _upperCase from 'lodash/upperCase';
 import { AvatarProps } from 'antd/lib/avatar';
 import { getAbsolutePath } from '@shared/utils/file-helper';
 
@@ -11,9 +12,11 @@ interface Props extends AvatarProps {
 const Avatar = React.memo((props: Props) => {
   const src = getAbsolutePath(props.src);
 
+  const name = useMemo(() => _upperCase(_head(props.name)), [props.name]);
+
   return (
     <AntdAvatar {...props} src={src}>
-      {_head(props.name)}
+      {name}
     </AntdAvatar>
   );
 });
