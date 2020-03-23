42637a8b4019af3ef21b9c90e4b611310805fe5d
moonrailgun <moonrailgun@gmail.com>
2020-03-23 22:30:54 +0800

    style: 移除Portal加载瞬间会出现滚动条的bug

--- src/web/components/LoadingSpinner.tsx
+++ src/web/components/LoadingSpinner.tsx
@@ -3,12 +3,14 @@ import './LoadingSpinner.scss';
 
 const LoadingSpinner: React.FC = React.memo(() => {
   return (
-    <div className="loading-spinner">
-      <div className="rect1" />
-      <div className="rect2" />
-      <div className="rect3" />
-      <div className="rect4" />
-      <div className="rect5" />
+    <div className="loading-spinner-container">
+      <div className="loading-spinner-inner">
+        <div className="rect1" />
+        <div className="rect2" />
+        <div className="rect3" />
+        <div className="rect4" />
+        <div className="rect5" />
+      </div>
     </div>
   );
 });
