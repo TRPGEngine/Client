82f7772f225a0790351b0ebaa3c64444ccb388be
moonrailgun <moonrailgun@gmail.com>
2020-03-24 19:33:32 +0800

    refactor(playground): 增加无限恐怖简版人物卡
    并对playground的示例做了一下中文翻译

--- src/playground/editor/example/index.ts
+++ src/playground/editor/example/index.ts
@@ -2,27 +2,31 @@
  * 示例代码
  */
 const exampleLayout = [
-  { label: 'Simple', value: require('./xml/simple.xml').default },
-  { label: 'Grid', value: require('./xml/grid.xml').default },
-  { label: 'Input', value: require('./xml/input.xml').default },
-  { label: 'Tabs', value: require('./xml/tabs.xml').default },
-  { label: 'Display', value: require('./xml/display.xml').default },
-  { label: 'Roll', value: require('./xml/roll.xml').default },
+  { label: '简单示例', value: require('./xml/simple.xml').default },
+  { label: '网格布局', value: require('./xml/grid.xml').default },
+  { label: '输入', value: require('./xml/input.xml').default },
+  { label: '标签页', value: require('./xml/tabs.xml').default },
+  { label: '显示', value: require('./xml/display.xml').default },
+  { label: '投骰', value: require('./xml/roll.xml').default },
   {
-    label: 'Use and Define',
+    label: '模板与复用',
     value: require('./xml/use-define.xml').default,
   },
   {
-    label: 'Var',
+    label: '变量',
     value: require('./xml/var.xml').default,
   },
   {
-    label: 'Function',
+    label: '方法',
     value: require('./xml/function.xml').default,
   },
   {
     label: 'CoC7',
     value: require('./xml/coc7.xml').default,
+  },
+  {
+    label: '无限简版',
+    value: require('./xml/wuxian-simple.xml').default,
   },
 ];
 
