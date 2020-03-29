e644e8c4e372dd3dffb7a83b84e17e6bc8053263
moonrailgun <moonrailgun@gmail.com>
2020-03-29 15:20:10 +0800

    test: move test file

--- src/shared/components/bbcode/__tests__/index.spec.ts
+++ src/shared/components/bbcode/__tests__/index.spec.ts
@@ -1,6 +1,6 @@
 import { preProcessLinkText } from '@src/shared/components/bbcode';
 
-describe('bbcode-parser common', () => {
+describe('bbcode common', () => {
   describe('preprocess text', () => {
     it('simple url parse', () => {
       const text = preProcessLinkText('http://baidu.com');
