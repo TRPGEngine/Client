ab3b67e8834be57fe0858c8e030bacb2e9d4922e
moonrailgun <moonrailgun@gmail.com>
2020-04-11 15:03:48 +0800

    feat(web): 增加团编辑地图的入口
    调整StandaloneWindow的关闭按钮阻止事件传播

--- src/web/components/StandaloneWindow.tsx
+++ src/web/components/StandaloneWindow.tsx
@@ -12,7 +12,7 @@ export interface StandaloneWindowConfig {
   title?: string;
   body: ReactNode;
   onMinimize?: () => void;
-  onClose?: () => void;
+  onClose?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
 const Container = styled.div`
@@ -151,7 +151,8 @@ StandaloneWindow.open = (config) => {
   ReactDOM.render(
     <StandaloneWindow
       {...config}
-      onClose={() => {
+      onClose={(e) => {
+        e.stopPropagation();
         destroy();
         _isFunction(config.onClose) && config.onClose();
       }}
--- src/web/containers/main/group/GroupMap.tsx
+++ src/web/containers/main/group/GroupMap.tsx
@@ -10,6 +10,8 @@ import { CreateGroupMap } from './modal/CreateGroupMap';
 import { showModal } from '@shared/redux/actions/ui';
 import styled from 'styled-components';
 import { showPortal } from '@web/redux/action/ui';
+import { getPortalUrl } from '@shared/utils/string-helper';
+import { useIsGroupManager } from '@redux/hooks/useGroup';
 
 const Container = styled.div`
   padding: 10px;
@@ -22,8 +24,21 @@ const MapItem = styled(Card)`
     display: flex;
     align-items: center;
 
-    > div {
+    > .info {
       flex: 1;
+    }
+
+    > .actions {
+      display: flex;
+      flex-direction: column;
+
+      > button {
+        margin-bottom: 4px;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
     }
   }
 `;
@@ -36,6 +51,7 @@ export const GroupMap: React.FC = TMemo(() => {
   const groupUUID = groupInfo.uuid;
   const mapList = groupInfo.maps;
   const dispatch = useTRPGDispatch();
+  const isGroupManager = useIsGroupManager(groupUUID);
 
   const handleCreateGroupMap = useCallback(() => {
     dispatch(showModal(<CreateGroupMap groupUUID={groupUUID} />));
@@ -53,6 +69,16 @@ export const GroupMap: React.FC = TMemo(() => {
     [groupUUID]
   );
 
+  const handleOpenGroupMapEdit = useCallback(
+    (mapUUID) => {
+      window.open(
+        getPortalUrl(`/group/${groupUUID}/map/${mapUUID}/edit`),
+        'newwindow'
+      );
+    },
+    [groupUUID]
+  );
+
   const mapListEl = useMemo(() => {
     if (_isEmpty(mapList)) {
       return <p>暂无地图</p>;
@@ -61,8 +87,17 @@ export const GroupMap: React.FC = TMemo(() => {
     return mapList.map((map) => {
       return (
         <MapItem key={map.uuid}>
-          <div>{map.name}</div>
-          <Button onClick={() => handleOpenGroupMap(map.uuid)}>打开地图</Button>
+          <div className="info">{map.name}</div>
+          <div className="actions">
+            {isGroupManager && (
+              <Button onClick={() => handleOpenGroupMapEdit(map.uuid)}>
+                编辑地图
+              </Button>
+            )}
+            <Button onClick={() => handleOpenGroupMap(map.uuid)}>
+              打开地图
+            </Button>
+          </div>
         </MapItem>
       );
     });
