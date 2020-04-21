808cf7d92ec1ab06deacc1ecbbfcf6a6301c49ef
moonrailgun <moonrailgun@gmail.com>
2020-04-21 17:13:46 +0800

    refactor(app): html渲染组件增加xss预处理方式一些不可预知的html代码

--- src/app/src/components/HTML.tsx
+++ src/app/src/components/HTML.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { TMemo } from '@shared/components/TMemo';
 import HTMLRender from 'react-native-render-html';
 import { Dimensions } from 'react-native';
+import xss from 'xss';
 
 interface Props {
   html: string;
 }
 export const HTML: React.FC<Props> = TMemo((props) => {
+  const html = useMemo(() => {
+    return xss.filterXSS(props.html, { css: false }); // 防止xss
+  }, [props.html]);
+
   return (
-    <HTMLRender
-      html={props.html}
-      imagesMaxWidth={Dimensions.get('window').width}
-    />
+    <HTMLRender html={html} imagesMaxWidth={Dimensions.get('window').width} />
   );
 });
 HTML.displayName = 'HTML';
