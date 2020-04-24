e70d204ff46f21752afa8c1d11c4202b7550c659
moonrailgun <moonrailgun@gmail.com>
2020-04-24 16:10:01 +0800

    feat(app): 增加了部分投骰图标。重绘了所有的投骰图标
    新增预设骰
    - 1d100
    - 1d12
    - 1d10

--- src/app/src/components/chat/QuickDiceBtn.tsx
+++ src/app/src/components/chat/QuickDiceBtn.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import TIcon from '../TComponent/TIcon';
 
 const DickBtn = styled.TouchableOpacity<{ isSelected?: boolean }>`
   width: 80px;
@@ -12,19 +11,12 @@ const DickBtn = styled.TouchableOpacity<{ isSelected?: boolean }>`
   border-color: ${(props) => (props.isSelected === true ? '#e67e22' : '#ccc')};
 `;
 
-const IconText = styled(TIcon)`
-  color: #ccc;
-  font-size: 32px;
-  text-align: center;
-  line-height: 48px;
-`;
-
 const BtnText = styled.Text`
   text-align: center;
 `;
 
 interface Props {
-  icon: string;
+  icon: React.ReactElement;
   text: string;
   isSelected: boolean;
   onPress?: () => void;
@@ -32,7 +24,7 @@ interface Props {
 const QuickDiceBtn = React.memo((props: Props) => {
   return (
     <DickBtn isSelected={props.isSelected} onPress={props.onPress}>
-      <IconText icon={props.icon} />
+      {props.icon}
       <BtnText>{props.text}</BtnText>
     </DickBtn>
   );
