52c1831d936e64d70d54005e4607c191d5aa5838
moonrailgun <moonrailgun@gmail.com>
2020-05-02 16:27:01 +0800

    refactor(app): 抽出ChatScreen的大部分逻辑

---
+++ src/app/src/screens/ChatScreen/Main.tsx
@@ -0,0 +1,355 @@
+import React, {
+  useCallback,
+  useState,
+  useRef,
+  useMemo,
+  useEffect,
+  Fragment,
+} from 'react';
+import { TMemo } from '@shared/components/TMemo';
+import { useMsgList } from '@redux/hooks/useMsgList';
+import _isArray from 'lodash/isArray';
+import _last from 'lodash/last';
+import _throttle from 'lodash/throttle';
+import _isNil from 'lodash/isNil';
+import { View, Text, Keyboard, TextInput } from 'react-native';
+import MsgList from './MsgList';
+import InputView, { UserMsgType } from './InputView';
+import { useCurrentUserInfo } from '@redux/hooks/useUser';
+import { useTRPGDispatch } from '@shared/hooks/useTRPGSelector';
+import { ChatType } from '@app/types/params';
+import { getMoreChatLog, sendMsg, addLoadingMsg } from '@redux/actions/chat';
+import { sendStartWriting } from '@shared/api/event';
+import config from '@shared/project.config';
+import { unemojify } from '@shared/utils/emoji';
+import { SendMsgPayload } from '@redux/types/chat';
+import { getCurrentGroupActor } from '@redux/helpers/group';
+import { MsgDataManager } from '@shared/utils/msg-helper';
+import QuickDiceModal from '@app/components/chat/QuickDiceModal';
+import { sendQuickDice } from '@redux/actions/dice';
+import EmotionPanel from '@app/components/chat/EmotionPanel';
+import styled from 'styled-components/native';
+import ExtraPanelItem from '@app/components/chat/ExtraPanelItem';
+import ImagePicker from 'react-native-image-picker';
+import { uploadChatimg } from '@shared/utils/image-uploader';
+
+const EXTRA_PANEL_HEIGHT = 220; // 额外面板高度
+
+const ExtraPanel = styled.View`
+  height: ${EXTRA_PANEL_HEIGHT};
+  background-color: white;
+  border-top-width: 1px;
+  border-top-color: #ccc;
+  flex-direction: row;
+`;
+
+interface Props {
+  converseUUID: string;
+  converseType: ChatType;
+}
+export const ChatScreenMain: React.FC<Props> = TMemo((props) => {
+  const selfInfo = useCurrentUserInfo();
+  const { list: msgList, nomore } = useMsgList(props.converseUUID, 'desc');
+  const dispatch = useTRPGDispatch();
+  const [isKeyboardShow, setIsKeyboardShow] = useState(false);
+  const [showExtraPanel, setShowExtraPanel] = useState(false);
+  const [showEmoticonPanel, setShowEmoticonPanel] = useState(false);
+  const [inputMsg, setInputMsg] = useState('');
+  const [msgType, setMsgType] = useState<UserMsgType>('normal');
+  const inputRef = useRef<TextInput>();
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setIsKeyboardShow(true);
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setIsKeyboardShow(false);
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  const dismissPanel = useCallback(() => {
+    // 收起所有面板
+    setShowExtraPanel(false);
+    setShowEmoticonPanel(false);
+  }, [setShowExtraPanel, setShowEmoticonPanel]);
+
+  const dismissAll = useCallback(() => {
+    Keyboard.dismiss();
+    dismissPanel();
+  }, [dismissPanel]);
+
+  const handleShowEmoticonPanel = useCallback(() => {
+    // 显示表情面板
+    if (showEmoticonPanel === true) {
+      setShowEmoticonPanel(false);
+      setShowExtraPanel(false);
+      inputRef.current.focus();
+    } else {
+      setShowEmoticonPanel(true);
+      setShowExtraPanel(false);
+      inputRef.current.blur();
+    }
+  }, [showEmoticonPanel, setShowEmoticonPanel, setShowExtraPanel, inputRef]);
+
+  const handleShowExtraPanel = useCallback(() => {
+    // 显示额外面板
+    if (showExtraPanel === true) {
+      setShowExtraPanel(false);
+      setShowEmoticonPanel(false);
+      inputRef.current.focus();
+    } else {
+      setShowExtraPanel(true);
+      setShowEmoticonPanel(false);
+      inputRef.current.blur();
+    }
+  }, [showExtraPanel, setShowEmoticonPanel, setShowExtraPanel, inputRef]);
+
+  const handleSwitchMsgType = useCallback(
+    (msgType: UserMsgType) => {
+      setMsgType(msgType);
+    },
+    [setMsgType]
+  );
+
+  /**
+   * 处理请求更多聊天记录事件
+   */
+  const handleRequestMoreChatLog = useCallback(() => {
+    // 获取时间最前的那条记录
+    const oldestDate = _last(msgList).date ?? new Date().toISOString();
+    const converseType = props.converseType;
+    dispatch(
+      getMoreChatLog(props.converseUUID, oldestDate, converseType === 'user')
+    );
+  }, [props.converseUUID, props.converseType]);
+
+  // 发送正在输入信号
+  // 增加一个2秒的节流防止频繁发送
+  const sendWritingThrottled = useMemo(
+    () =>
+      _throttle(
+        () => {
+          sendStartWriting('user', props.converseUUID);
+        },
+        config.chat.isWriting.throttle,
+        { leading: true, trailing: false }
+      ),
+    [props.converseUUID]
+  );
+
+  const handleChange = useCallback(
+    (text: string) => {
+      setInputMsg(text);
+
+      if (props.converseType === 'user') {
+        // 通知服务器告知converseUUID当前用户正在输入
+        sendWritingThrottled();
+      }
+    },
+    [setInputMsg, props.converseType, sendWritingThrottled]
+  );
+
+  /**
+   * 向服务器发送信息
+   * @param {string} message 要发送的文本
+   */
+  const sendMsgToServer = useCallback(
+    (message: string) => {
+      const uuid = props.converseUUID;
+      const converseType = props.converseType;
+
+      if (!message) {
+        console.warn('require message to send');
+      }
+
+      // this.props.onSendMsg(message, type);
+      if (!!uuid) {
+        message = unemojify(message); // 转成标准文本
+
+        const payload: SendMsgPayload = {
+          message,
+          type: msgType ?? 'normal',
+          is_public: false,
+          is_group: false,
+        };
+
+        if (converseType === 'user' || converseType === 'system') {
+          // 如果是1对1消息
+          dispatch(sendMsg(uuid, payload));
+        } else if (converseType === 'group') {
+          // 如果是群组消息
+          payload.converse_uuid = uuid;
+          payload.is_public = true;
+          payload.is_group = true;
+
+          const currentGroupActor = getCurrentGroupActor(uuid);
+          const msgDataManager = new MsgDataManager();
+          if (!_isNil(currentGroupActor)) {
+            msgDataManager.setGroupActorInfo(currentGroupActor);
+
+            payload.data = msgDataManager.toJS();
+          }
+          dispatch(sendMsg(null, payload));
+        }
+      }
+    },
+    [props.converseUUID, props.converseType, msgType]
+  );
+
+  const handleSendMsg = useCallback(() => {
+    const message = inputMsg.trim();
+    if (!!message) {
+      sendMsgToServer(message);
+      setInputMsg('');
+    }
+  }, [inputMsg, setInputMsg, sendMsgToServer]);
+
+  const [showQuickDiceModal, setShowQuickDiceModal] = useState(false);
+  const handleSendQuickDice = useCallback(
+    (diceExp: string) => {
+      const uuid = props.converseUUID;
+      const converseType = props.converseType;
+      const isGroup = converseType === 'group';
+      dispatch(sendQuickDice(uuid, isGroup, diceExp));
+      setShowQuickDiceModal(false);
+    },
+    [props.converseUUID, props.converseType, setShowQuickDiceModal]
+  );
+  const modalEl = useMemo(() => {
+    return (
+      <Fragment>
+        <QuickDiceModal
+          visible={showQuickDiceModal}
+          onClose={() => setShowQuickDiceModal(false)}
+          onSend={handleSendQuickDice}
+        />
+      </Fragment>
+    );
+  }, [showQuickDiceModal, setShowQuickDiceModal, handleSendQuickDice]);
+
+  const emotionPanelEl = useMemo(() => {
+    // 表情面板的渲染函数
+    return (
+      <EmotionPanel
+        onSelectEmoji={(code) => {
+          // 增加到输入框
+          const newMsg = inputMsg + code;
+          setInputMsg(newMsg);
+        }}
+        onSelectEmotion={(emotionUrl) =>
+          sendMsgToServer(`[img]${emotionUrl}[/img]`)
+        }
+      />
+    );
+  }, [inputMsg, setInputMsg, sendMsgToServer]);
+
+  const handleSendImage = useCallback(() => {
+    // 额外面板的发送图片功能
+    ImagePicker.launchImageLibrary(
+      {
+        mediaType: 'photo',
+        allowsEditing: true,
+        maxWidth: 1200,
+        maxHeight: 1200,
+      },
+      (response) => {
+        dismissAll();
+
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+        } else {
+          const file = {
+            uri: response.uri,
+            type: response.type,
+            name: response.fileName,
+          } as any;
+
+          const uuid = props.converseUUID;
+          dispatch(
+            addLoadingMsg(uuid, ({ updateProgress, removeLoading }) => {
+              uploadChatimg(file, {
+                onUploadProgress(percent) {
+                  updateProgress(percent); // 更新loading百分比
+                },
+              }).then((imageUrl) => {
+                removeLoading(); // 移除loading消息
+                const message = `[img]${imageUrl}[/img]`;
+                console.log('message', message);
+                sendMsgToServer(message);
+              });
+            })
+          );
+        }
+      }
+    );
+  }, [dismissAll, props.converseUUID, dispatch, sendMsgToServer]);
+  const extraPanelEl = useMemo(() => {
+    return (
+      <ExtraPanel>
+        <ExtraPanelItem
+          text="发送图片"
+          icon="&#xe621;"
+          onPress={handleSendImage}
+        />
+        <ExtraPanelItem
+          text="投骰"
+          icon="&#xe629;"
+          onPress={() => setShowQuickDiceModal(true)}
+        />
+      </ExtraPanel>
+    );
+  }, [handleSendImage, setShowQuickDiceModal]);
+
+  if (!_isArray(msgList)) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <MsgList
+        msgList={msgList}
+        selfInfo={selfInfo}
+        nomore={nomore}
+        onTouchStart={dismissAll}
+        onRequestMoreChatLog={handleRequestMoreChatLog}
+      />
+      <InputView
+        inputRef={inputRef}
+        value={inputMsg}
+        msgType={msgType}
+        onChange={handleChange}
+        onSendMsg={handleSendMsg}
+        onFocus={dismissPanel}
+        onShowEmoticonPanel={handleShowEmoticonPanel}
+        onShowExtraPanel={handleShowExtraPanel}
+        onChangeMsgType={handleSwitchMsgType}
+      />
+      {showEmoticonPanel &&
+        !showExtraPanel &&
+        !isKeyboardShow &&
+        emotionPanelEl}
+      {showExtraPanel && !showEmoticonPanel && !isKeyboardShow && extraPanelEl}
+      {modalEl}
+    </View>
+  );
+});
+ChatScreenMain.displayName = 'ChatScreenMain';
--- src/app/src/screens/ChatScreen/index.tsx
+++ src/app/src/screens/ChatScreen/index.tsx
@@ -1,26 +1,9 @@
-import React, { Fragment } from 'react';
-import { connect, DispatchProp } from 'react-redux';
-import {
-  View,
-  Text,
-  TextInput,
-  Keyboard,
-  EmitterSubscription,
-} from 'react-native';
-import ImagePicker from 'react-native-image-picker';
+import React from 'react';
+import { connect } from 'react-redux';
+import { View } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 import { TIcon } from '@app/components/TComponent';
-import {
-  sendMsg,
-  getMoreChatLog,
-  addLoadingMsg,
-  clearSelectedConverse,
-} from '@shared/redux/actions/chat';
-import ExtraPanelItem from '@app/components/chat/ExtraPanelItem';
-import EmotionPanel from '@app/components/chat/EmotionPanel';
-import QuickDiceModal from '@app/components/chat/QuickDiceModal';
-import { uploadChatimg } from '@shared/utils/image-uploader';
-import { unemojify } from '@shared/utils/emoji';
+import { clearSelectedConverse } from '@shared/redux/actions/chat';
 import _get from 'lodash/get';
 import _isNil from 'lodash/isNil';
 import _isArray from 'lodash/isArray';
@@ -28,28 +11,11 @@ import _throttle from 'lodash/throttle';
 import _orderBy from 'lodash/orderBy';
 import _last from 'lodash/last';
 import { ChatParams } from '../../types/params';
-
-import styled from 'styled-components/native';
-import { sendQuickDice } from '@src/shared/redux/actions/dice';
-import InputView, { UserMsgType } from './InputView';
-import MsgList from './MsgList';
+import { UserMsgType } from './InputView';
 import { TRPGState, TRPGDispatchProp } from '@src/shared/redux/types/__all__';
 import { clearSelectGroup } from '@src/shared/redux/actions/group';
-import { sendStartWriting } from '@src/shared/api/event';
-import config from '@src/shared/project.config';
-import { getCurrentGroupActor } from '@redux/helpers/group';
-import { MsgListType, SendMsgPayload } from '@redux/types/chat';
-import { MsgDataManager } from '@shared/utils/msg-helper';
-
-const EXTRA_PANEL_HEIGHT = 220; // 额外面板高度
-
-const ExtraPanel = styled.View`
-  height: ${EXTRA_PANEL_HEIGHT};
-  background-color: white;
-  border-top-width: 1px;
-  border-top-color: #ccc;
-  flex-direction: row;
-`;
+import { MsgListType } from '@redux/types/chat';
+import { ChatScreenMain } from './Main';
 
 type Params = ChatParams & { headerRightFunc?: () => void };
 
@@ -90,30 +56,6 @@ class ChatScreen extends React.Component<Props, State> {
     };
   };
 
-  state: Readonly<State> = {
-    inputMsg: '',
-    msgType: 'normal',
-    showExtraPanel: false,
-    showEmoticonPanel: false,
-    isKeyboardShow: false,
-    showQuickDiceModal: false,
-  };
-
-  keyboardDidShowListener: EmitterSubscription;
-  keyboardDidHideListener: EmitterSubscription;
-  inputRef = React.createRef<TextInput>();
-
-  // 发送正在输入信号
-  // 增加一个2秒的节流防止频繁发送
-  sendWritingThrottled = _throttle(
-    () => {
-      const converseUUID = this.props.navigation.getParam('uuid');
-      sendStartWriting('user', converseUUID);
-    },
-    config.chat.isWriting.throttle,
-    { leading: true, trailing: false }
-  );
-
   get converseType() {
     return this.props.navigation.getParam('type', 'user');
   }
@@ -136,23 +78,9 @@ class ChatScreen extends React.Component<Props, State> {
         }
       },
     });
-    this.keyboardDidShowListener = Keyboard.addListener(
-      'keyboardDidShow',
-      () => {
-        this.setState({ isKeyboardShow: true });
-      }
-    );
-    this.keyboardDidHideListener = Keyboard.addListener(
-      'keyboardDidHide',
-      () => {
-        this.setState({ isKeyboardShow: false });
-      }
-    );
   }
 
   componentWillUnmount() {
-    this.keyboardDidShowListener.remove();
-    this.keyboardDidHideListener.remove();
     if (this.converseType === 'group') {
       this.props.dispatch(clearSelectGroup());
     } else {
@@ -169,269 +97,13 @@ class ChatScreen extends React.Component<Props, State> {
     }
   }
 
-  dismissAll = () => {
-    Keyboard.dismiss();
-    this.setState({
-      showExtraPanel: false,
-      showEmoticonPanel: false,
-    });
-  };
-
-  /**
-   * 向服务器发送信息
-   * @param {string} message 要发送的文本
-   */
-  sendMsg(message: string) {
-    const uuid = this.props.navigation.getParam('uuid', '');
-    const converseType = this.converseType;
-    if (!!message) {
-      // this.props.onSendMsg(message, type);
-      if (!!uuid) {
-        message = unemojify(message); // 转成标准文本
-
-        const payload: SendMsgPayload = {
-          message,
-          type: this.state.msgType ?? 'normal',
-          is_public: false,
-          is_group: false,
-        };
-
-        if (converseType === 'user' || converseType === 'system') {
-          // 如果是1对1消息
-          this.props.dispatch(sendMsg(uuid, payload));
-        } else if (converseType === 'group') {
-          // 如果是群组消息
-          payload.converse_uuid = uuid;
-          payload.is_public = true;
-          payload.is_group = true;
-
-          const currentGroupActor = getCurrentGroupActor(
-            this.props.selectedConverseUUID
-          );
-          const msgDataManager = new MsgDataManager();
-          if (!_isNil(currentGroupActor)) {
-            msgDataManager.setGroupActorInfo(currentGroupActor);
-
-            payload.data = msgDataManager.toJS();
-          }
-          this.props.dispatch(sendMsg(null, payload));
-        }
-      }
-    } else {
-      console.warn('require message to send');
-    }
-  }
-
-  /**
-   * 处理请求更多聊天记录事件
-   */
-  handleRequestMoreChatLog = () => {
-    const msgList = this.props.msgList;
-    // 获取时间最前的那条记录
-    const oldestDate = _last(msgList).date ?? new Date().toISOString();
-    const { selectedConverseUUID } = this.props;
-    const converseType = this.converseType;
-    this.props.dispatch(
-      getMoreChatLog(selectedConverseUUID, oldestDate, converseType === 'user')
-    );
-  };
-
-  handleFocus = () => {
-    // 输入框focus时收起所有面板
-    this.setState({
-      showExtraPanel: false,
-      showEmoticonPanel: false,
-    });
-  };
-
-  handleChange = (text: string) => {
-    this.setState({ inputMsg: text });
-
-    if (this.converseType === 'user') {
-      // 通知服务器告知converseUUID当前用户正在输入
-      this.sendWritingThrottled();
-    }
-  };
-
-  handleSendMsg = () => {
-    let message = this.state.inputMsg.trim();
-    if (!!message) {
-      this.sendMsg(message);
-      this.setState({ inputMsg: '' });
-    }
-  };
-
-  // 显示表情面板
-  handleShowEmoticonPanel = () => {
-    if (this.state.showEmoticonPanel === true) {
-      this.setState({ showEmoticonPanel: false, showExtraPanel: false });
-      this.inputRef.current.focus();
-    } else {
-      this.setState({ showEmoticonPanel: true, showExtraPanel: false });
-      this.inputRef.current.blur();
-    }
-  };
-
-  // 显示额外面板
-  handleShowExtraPanel = () => {
-    if (this.state.showExtraPanel === true) {
-      this.setState({ showExtraPanel: false, showEmoticonPanel: false });
-      this.inputRef.current.focus();
-    } else {
-      this.setState({ showExtraPanel: true, showEmoticonPanel: false });
-      this.inputRef.current.blur();
-    }
-  };
-
-  // 切换当前消息类型
-  handleSwitchMsgType = (msgType: UserMsgType) => {
-    this.setState({ msgType });
-  };
-
-  /**
-   * 额外面板的发送图片功能
-   */
-  handleSendImage = () => {
-    ImagePicker.launchImageLibrary(
-      {
-        mediaType: 'photo',
-        allowsEditing: true,
-        maxWidth: 1200,
-        maxHeight: 1200,
-      },
-      (response) => {
-        this.dismissAll();
-
-        if (response.didCancel) {
-          console.log('User cancelled image picker');
-        } else if (response.error) {
-          console.log('ImagePicker Error: ', response.error);
-        } else if (response.customButton) {
-          console.log('User tapped custom button: ', response.customButton);
-        } else {
-          const selfUUID = this.props.selfUUID;
-          const file = {
-            uri: response.uri,
-            type: response.type,
-            name: response.fileName,
-          } as any;
-
-          const uuid = this.props.navigation.getParam('uuid', '');
-          this.props.dispatch(
-            addLoadingMsg(uuid, ({ updateProgress, removeLoading }) => {
-              uploadChatimg(file, {
-                onUploadProgress(percent) {
-                  updateProgress(percent); // 更新loading百分比
-                },
-              }).then((imageUrl) => {
-                removeLoading(); // 移除loading消息
-                const message = `[img]${imageUrl}[/img]`;
-                console.log('message', message);
-                this.sendMsg(message);
-              });
-            })
-          );
-        }
-      }
-    );
-  };
-
-  handleSendQuickDice = (diceExp: string) => {
-    const uuid = this.props.navigation.getParam('uuid', '');
-    const converseType = this.converseType;
-    const isGroup = converseType === 'group';
-    this.props.dispatch(sendQuickDice(uuid, isGroup, diceExp));
-    this.setState({ showQuickDiceModal: false });
-  };
-
-  // 表情面板的渲染函数
-  getEmotionPanel() {
+  render() {
     return (
-      <EmotionPanel
-        onSelectEmoji={(code) => {
-          // 增加到输入框
-          const newMsg = this.state.inputMsg + code;
-          this.setState({ inputMsg: newMsg });
-        }}
-        onSelectEmotion={(emotionUrl) =>
-          this.sendMsg(`[img]${emotionUrl}[/img]`)
-        }
+      <ChatScreenMain
+        converseUUID={this.props.selectedConverseUUID}
+        converseType={this.converseType}
       />
     );
-  }
-
-  // 额外面板的渲染函数
-  getExtraPanel() {
-    return (
-      <ExtraPanel>
-        <ExtraPanelItem
-          text="发送图片"
-          icon="&#xe621;"
-          onPress={this.handleSendImage}
-        />
-        <ExtraPanelItem
-          text="投骰"
-          icon="&#xe629;"
-          onPress={() => this.setState({ showQuickDiceModal: true })}
-        />
-      </ExtraPanel>
-    );
-  }
-
-  getModal() {
-    return (
-      <Fragment>
-        <QuickDiceModal
-          visible={this.state.showQuickDiceModal}
-          onClose={() => this.setState({ showQuickDiceModal: false })}
-          onSend={this.handleSendQuickDice}
-        />
-      </Fragment>
-    );
-  }
-
-  render() {
-    const { msgList } = this.props;
-
-    if (_isArray(msgList)) {
-      return (
-        <View style={{ flex: 1 }}>
-          <MsgList
-            msgList={msgList}
-            selfInfo={this.props.selfInfo}
-            nomore={this.props.nomore}
-            onTouchStart={this.dismissAll}
-            onRequestMoreChatLog={this.handleRequestMoreChatLog}
-          />
-          <InputView
-            inputRef={this.inputRef}
-            value={this.state.inputMsg}
-            msgType={this.state.msgType}
-            onChange={this.handleChange}
-            onSendMsg={this.handleSendMsg}
-            onFocus={this.handleFocus}
-            onShowEmoticonPanel={this.handleShowEmoticonPanel}
-            onShowExtraPanel={this.handleShowExtraPanel}
-            onChangeMsgType={this.handleSwitchMsgType}
-          />
-          {this.state.showEmoticonPanel &&
-            !this.state.showExtraPanel &&
-            !this.state.isKeyboardShow &&
-            this.getEmotionPanel()}
-          {this.state.showExtraPanel &&
-            !this.state.showEmoticonPanel &&
-            !this.state.isKeyboardShow &&
-            this.getExtraPanel()}
-          {this.getModal()}
-        </View>
-      );
-    } else {
-      return (
-        <View>
-          <Text>Loading...</Text>
-        </View>
-      );
-    }
   }
 }
 
@@ -450,23 +122,8 @@ export default connect((state: TRPGState, ownProps: Props) => {
     );
   }
 
-  const msgList = _get(state, [
-    'chat',
-    'converses',
-    selectedConverseUUID,
-    'msgList',
-  ]);
-
   return {
     selectedConverseUUID,
-    selfInfo: state.user.info,
-    selfUUID: state.user.info.uuid,
-    msgList:
-      msgList && _orderBy(msgList, (item) => new Date(item.date), 'desc'),
-    usercache: state.cache.user,
-    nomore:
-      _get(state, ['chat', 'converses', selectedConverseUUID, 'nomore']) ??
-      false,
     isWriting,
   };
 })(ChatScreen);
