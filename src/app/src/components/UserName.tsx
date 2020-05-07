7608328d5d6bdcc5e0c2e6a8c9bc3ba24f11f77a
moonrailgun <moonrailgun@gmail.com>
2020-05-07 11:49:57 +0800

    refactor(app): 增加UserName组件用于根据UUID获取用于显示的用户名

---
+++ src/app/src/components/UserName.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { TMemo } from '@shared/components/TMemo';
+import { useUserName } from '@redux/hooks/useUser';
+
+interface Props {
+  uuid: string;
+}
+export const UserName: React.FC<Props> = TMemo((props) => {
+  const displayName = useUserName(props.uuid);
+
+  return <Text>{displayName}</Text>;
+});
+UserName.displayName = 'UserName';
