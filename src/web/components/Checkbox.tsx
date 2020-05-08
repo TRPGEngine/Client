b891a97df0bfce45b22d0ee35a0ae91bb8b09ffa
moonrailgun <moonrailgun@gmail.com>
2020-05-08 14:45:08 +0800

    perf(web): 优化Checkbox

--- src/web/components/Checkbox.tsx
+++ src/web/components/Checkbox.tsx
@@ -5,15 +5,19 @@ interface Props {
   value: boolean;
   onChange: (value: boolean) => void;
 }
-class Checkbox extends React.Component<Props> {
+class Checkbox extends React.PureComponent<Props> {
+  get value() {
+    return Boolean(this.props.value);
+  }
+
   handleClick = () => {
-    this.props.onChange && this.props.onChange(!this.props.value);
+    this.props.onChange && this.props.onChange(!this.value);
   };
 
   render() {
     return (
-      <div className="check-box" onClick={() => this.handleClick()}>
-        <label className={this.props.value ? 'checked' : 'unchecked'} />
+      <div className="check-box" onClick={this.handleClick}>
+        <label className={this.value ? 'checked' : 'unchecked'} />
       </div>
     );
   }
