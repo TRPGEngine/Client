12671232386cecd2186ece699c9dc6e7c9ce15af
moonrailgun <moonrailgun@gmail.com>
2020-05-31 16:02:56 +0800

    chore: 修复一些新版react-native带来的问题

--- src/app/android/app/src/main/java/com/moonrailgun/trpg/MainApplication.java
+++ src/app/android/app/src/main/java/com/moonrailgun/trpg/MainApplication.java
@@ -71,8 +71,9 @@ public class MainApplication extends Application implements ReactApplication {
       // );
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+      // Packages that cannot be autolinked yet can be added manually here
+      packages.add(new DplusReactPackage());
+      packages.add(new TRPGPackage());
       return packages;
     }
 
---
+++ src/app/build/tmp-quickfix.md
@@ -0,0 +1,3 @@
+## 修复_this.view._component.measureInWindow的问题
+
+[https://stackoverflow.com/questions/60944091/taskqueue-error-with-task-undefined-is-not-an-object-evaluating-this-view](https://stackoverflow.com/questions/60944091/taskqueue-error-with-task-undefined-is-not-an-object-evaluating-this-view)
