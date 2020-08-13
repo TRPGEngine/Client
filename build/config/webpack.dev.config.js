3f9c8d930ff454fc45206a05bbb273e4ea0f1595
moonrailgun <moonrailgun@gmail.com>
2020-08-13 21:27:18 +0800

    chore: update webpack config

--- build/config/webpack.dev.config.js
+++ build/config/webpack.dev.config.js
@@ -10,6 +10,9 @@ module.exports = webpackMerge({}, base, {
     compress: true, // 启用Gzip压缩
     historyApiFallback: true, // 为404页启用多个路径
     hot: true, // 模块热更新，配置HotModuleReplacementPlugin
+    inline: true,
+    host: '0.0.0.0',
+    port: 8089, // 监听端口号
     https: false, // 适用于ssl安全证书网站
     noInfo: true, // 只在热加载错误和警告
     proxy: {
