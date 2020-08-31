c17a6ecb253f68bab0e1e2ebb45825a39e747747
moonrailgun <moonrailgun@gmail.com>
2020-08-31 18:06:19 +0800

    refactor(web): new UI: add set alert

--- src/shared/manager/ui.ts
+++ src/shared/manager/ui.ts
@@ -8,3 +8,11 @@ type ToastsType = 'info' | 'success' | 'error' | 'warning';
 export const [showToasts, setToasts] = buildRegFn<
   (message: string, type?: ToastsType) => void
 >('toasts');
+
+interface AlertOptions {
+  message: string;
+  onConfirm?: () => void | Promise<void>;
+}
+export const [showAlert, setAlert] = buildRegFn<
+  (options: AlertOptions) => void
+>('alert');
--- src/web/init.tsx
+++ src/web/init.tsx
@@ -1,10 +1,20 @@
-import { setToasts } from '@shared/manager/ui';
-import { message } from 'antd';
+import { setToasts, setAlert } from '@shared/manager/ui';
+import { message, Modal } from 'antd';
+import _isFunction from 'lodash/isFunction';
 
 setToasts((msg, type = 'info') => {
   message.open({
     type,
     duration: 3,
     content: msg,
+  });
+});
+
+setAlert((options) => {
+  Modal.confirm({
+    content: options.message,
+    onOk: async () => {
+      _isFunction(options.onConfirm) && (await options.onConfirm());
+    },
   });
 });
