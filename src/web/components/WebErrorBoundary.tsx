3798c3d33dc513b51181490f3d260d67dfc4589c
moonrailgun <moonrailgun@gmail.com>
2020-09-08 14:55:13 +0800

    refactor(web): new UI: 笔记面板的显示

--- src/web/components/WebErrorBoundary.tsx
+++ src/web/components/WebErrorBoundary.tsx
@@ -6,31 +6,34 @@ import ErrorBoundary, {
 } from '@shared/components/ErrorBoundary';
 import config from '@shared/project.config';
 import { sendErrorReport } from '@shared/utils/error-report';
+import { AlertErrorView } from './AlertErrorView';
 
 /**
  * web页面通用的错误处理容器
  * 应当给 web portal playground 通用
  */
-export const WebErrorBoundary: React.FC<{
-  renderError: RenderErrorComponent;
-}> = TMemo((props) => {
-  const handleCatchError: HandleCatchErrorFn = useCallback((error, info) => {
-    console.warn('捕获错误, 等待发送错误报告\n', error, info);
-    sendErrorReport({
-      message: String(error),
-      stack: String(info.componentStack),
-      version: config.version,
-    });
-    import('../utils/sentry').then((module) => module.error(error));
-  }, []);
+interface WebErrorBoundaryProps {
+  renderError?: RenderErrorComponent;
+}
+export const WebErrorBoundary: React.FC<WebErrorBoundaryProps> = TMemo(
+  (props) => {
+    const { renderError = AlertErrorView } = props;
 
-  return (
-    <ErrorBoundary
-      onCatchError={handleCatchError}
-      renderError={props.renderError}
-    >
-      {props.children}
-    </ErrorBoundary>
-  );
-});
+    const handleCatchError: HandleCatchErrorFn = useCallback((error, info) => {
+      console.warn('捕获错误, 等待发送错误报告\n', error, info);
+      sendErrorReport({
+        message: String(error),
+        stack: String(info.componentStack),
+        version: config.version,
+      });
+      import('../utils/sentry').then((module) => module.error(error));
+    }, []);
+
+    return (
+      <ErrorBoundary onCatchError={handleCatchError} renderError={renderError}>
+        {props.children}
+      </ErrorBoundary>
+    );
+  }
+);
 WebErrorBoundary.displayName = 'WebErrorBoundary';
