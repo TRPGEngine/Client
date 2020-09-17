10e88782de2f39bbf460206907a95b00aa92a415
moonrailgun <moonrailgun@gmail.com>
2020-09-17 15:01:00 +0800

    refactor(web): 对普通成员隐藏邀请成员与创建面板的操作

--- src/web/routes/Main/Content/Group/GroupHeader.tsx
+++ src/web/routes/Main/Content/Group/GroupHeader.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { TMemo } from '@shared/components/TMemo';
 import { SectionHeader } from '@web/components/SectionHeader';
 import { Menu } from 'antd';
-import { useJoinedGroupInfo } from '@redux/hooks/group';
+import { useIsGroupManager, useJoinedGroupInfo } from '@redux/hooks/group';
 import { useCurrentUserUUID } from '@redux/hooks/user';
 import _isNil from 'lodash/isNil';
 import { useGroupHeaderAction } from './useGroupHeaderAction';
@@ -16,6 +16,7 @@ export const GroupHeader: React.FC<GroupHeaderProps> = TMemo((props) => {
   const groupInfo = useJoinedGroupInfo(groupUUID);
   const currentUserUUID = useCurrentUserUUID();
   const { t } = useTranslation();
+  const isGroupManager = useIsGroupManager(groupUUID);
 
   const {
     handleShowGroupInfo,
@@ -31,8 +32,14 @@ export const GroupHeader: React.FC<GroupHeaderProps> = TMemo((props) => {
   const menu = (
     <Menu>
       <Menu.Item onClick={handleShowGroupInfo}>{t('查看详情')}</Menu.Item>
-      <Menu.Item onClick={handleShowInvite}>{t('邀请成员')}</Menu.Item>
-      <Menu.Item onClick={handleCreateGroupPanel}>{t('创建面板')}</Menu.Item>
+      {isGroupManager && (
+        <Fragment>
+          <Menu.Item onClick={handleShowInvite}>{t('邀请成员')}</Menu.Item>
+          <Menu.Item onClick={handleCreateGroupPanel}>
+            {t('创建面板')}
+          </Menu.Item>
+        </Fragment>
+      )}
       <Menu.Item danger={true} onClick={handleQuitGroup}>
         {currentUserUUID === groupInfo.owner_uuid ? t('解散团') : t('退出团')}
       </Menu.Item>
