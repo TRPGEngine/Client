9e055ba7ec8ade6e0df8809fbfa09ea2602302c4
moonrailgun <moonrailgun@gmail.com>
2020-10-13 10:57:51 +0800

    test: add bbcode image testcase

--- src/web/components/editor/utils/serialize/__tests__/bbcode.spec.ts
+++ src/web/components/editor/utils/serialize/__tests__/bbcode.spec.ts
@@ -1,7 +1,7 @@
 import { serializeToBBCode } from '../bbcode';
 
 describe('serializeToBBCode', () => {
-  test('parse at', () => {
+  test('parse mention', () => {
     const res = serializeToBBCode({
       children: [
         { text: '测试' },
@@ -20,5 +20,21 @@ describe('serializeToBBCode', () => {
     expect(res).toBe(
       '测试[at uuid=232a7ea0-5521-11e9-a029-7742fd774d52]admin1[/at]'
     );
+  });
+
+  test('parse image', () => {
+    const res = serializeToBBCode({
+      children: [
+        { text: '测试' },
+        {
+          type: 'image',
+          url: 'http://image.baidu.com',
+          children: [{ text: '' }],
+        },
+        { text: '' },
+      ],
+    });
+
+    expect(res).toBe('测试[img]http://image.baidu.com[/img]');
   });
 });
