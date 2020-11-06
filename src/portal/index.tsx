aa2876dbffe5445e8cbdb397a3119d22eb089930
moonrailgun <moonrailgun@gmail.com>
2020-11-06 15:33:55 +0800

    fix(portal): 修复portal端战报汇报列表无法打开的bug

--- src/portal/index.tsx
+++ src/portal/index.tsx
@@ -7,17 +7,20 @@ import zhCN from 'antd/es/locale/zh_CN';
 import { WebErrorBoundary } from '@web/components/WebErrorBoundary';
 import { AlertErrorView } from '@web/components/AlertErrorView';
 import { ThemeContextProvider } from '@shared/context/ThemeContext';
+import { PortalReduxProvider } from './components/PortalReduxProvider';
 
 import '@web/assets/css/iconfont.css';
 import './index.css';
 
 ReactDOM.render(
   <WebErrorBoundary renderError={AlertErrorView}>
-    <ThemeContextProvider>
-      <ConfigProvider locale={zhCN}>
-        <App />
-      </ConfigProvider>
-    </ThemeContextProvider>
+    <PortalReduxProvider>
+      <ThemeContextProvider>
+        <ConfigProvider locale={zhCN}>
+          <App />
+        </ConfigProvider>
+      </ThemeContextProvider>
+    </PortalReduxProvider>
   </WebErrorBoundary>,
   document.querySelector('#app')
 );
