7d2a1ad3f28e9125bd132e16a756c3ba3eead792
moonrailgun <moonrailgun@gmail.com>
2020-11-11 15:28:09 +0800

    perf(web): portal报错时带上用户的上下文

--- src/web/utils/sentry.ts
+++ src/web/utils/sentry.ts
@@ -1,3 +1,4 @@
+import type { CaptureContext } from '@sentry/types';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import Config from 'config';
@@ -6,6 +7,7 @@ import _once from 'lodash/once';
 import _isNil from 'lodash/isNil';
 import config from '@shared/project.config';
 import { UserInfo } from '@redux/types/user';
+import { getJWTUserInfo } from '@shared/utils/jwt-helper';
 
 const environment = config.environment;
 
@@ -32,7 +34,7 @@ const initSentry = _once(() => {
  * 汇报错误
  * @param err 错误
  */
-export function error(err: Error | string) {
+export async function error(err: Error | string) {
   initSentry(); // 确保Sentry被初始化
 
   let fn;
@@ -41,7 +43,14 @@ export function error(err: Error | string) {
   } else {
     fn = Sentry.captureException.bind(Sentry);
   }
-  const sentryId = fn(err);
+  const context: CaptureContext = {
+    contexts: {
+      token: {
+        ...(await getJWTUserInfo()),
+      },
+    },
+  };
+  const sentryId = fn(err, context);
   console.warn('error: sentryId', sentryId);
 }
 
