e13d29b49be3205ae4d5fe983deac4400d0488b7
moonrailgun <moonrailgun@gmail.com>
2020-11-16 15:21:24 +0800

    test: 增加测试用例用于测试useGroupUnread

---
+++ src/shared/redux/__tests__/example/chat.ts
@@ -0,0 +1,19 @@
+import { ChatStateConverse } from '@redux/types/chat';
+
+/**
+ * 构建测试用的redux数据
+ */
+export function buildTestChatConverse(
+  info?: Partial<ChatStateConverse>
+): ChatStateConverse {
+  return {
+    uuid: 'test-chat-converse-uuid',
+    type: 'group',
+    name: 'test converse',
+    lastMsg: '',
+    lastTime: new Date().valueOf(),
+    msgList: [],
+
+    ...info,
+  };
+}
---
+++ src/shared/redux/__tests__/example/group.ts
@@ -0,0 +1,39 @@
+import { GroupChannel, GroupInfo } from '@redux/types/group';
+
+/**
+ * 构建测试用的redux数据
+ */
+export function buildTestGroupInfo(info?: Partial<GroupInfo>): GroupInfo {
+  return {
+    id: 0,
+    uuid: 'test-group-uuid',
+    type: 'group',
+    name: 'test group',
+    sub_name: 'test sub name',
+    desc: 'any',
+    avatar: '',
+    max_member: 50,
+    allow_search: true,
+    creator_uuid: '',
+    owner_uuid: '',
+
+    ...info,
+  };
+}
+
+/**
+ * 构建测试用的redux数据
+ */
+export function buildTestGroupChannelInfo(
+  info?: Partial<GroupChannel>
+): GroupChannel {
+  return {
+    uuid: 'test group channel',
+    name: 'test group channel name',
+    desc: '',
+    visible: 'all',
+    members: [],
+
+    ...info,
+  };
+}
