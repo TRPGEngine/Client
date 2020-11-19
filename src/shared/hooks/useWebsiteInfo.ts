0035813be21c69361ad90b4ed3e2dd04acfaefc6
moonrailgun <moonrailgun@gmail.com>
2020-11-19 22:21:07 +0800

    fix: 修复解析bbcode 对于自定义描述文本的url的信息的获取错误的问题
    并增加了请求前进行url校验

--- src/shared/hooks/useWebsiteInfo.ts
+++ src/shared/hooks/useWebsiteInfo.ts
@@ -6,6 +6,7 @@ import { preProcessText } from '@shared/components/bbcode';
 import bbcodeParser from '@shared/components/bbcode/parser';
 import { AstNodeObj } from '@shared/components/bbcode/type';
 import { fetchWebsiteInfo, WebsiteInfo } from '@shared/model/info';
+import { getUrlTagRealUrl } from '@shared/components/bbcode/utils';
 
 interface WebsiteInfoWithUrl extends WebsiteInfo {
   url: string;
@@ -28,7 +29,8 @@ export function useWebsiteInfo(rawMessage: string): UseWebsiteInfoRet {
     // 不直接通过正则获取url是因为可能会获取到图片url之类的其他地图
     const message = preProcessText(rawMessage);
 
-    if (message.indexOf('[url]') >= 0) {
+    if (message.indexOf('[url') >= 0) {
+      // 有url的话
       const ast = bbcodeParser.parse(message);
       const urlTag = ast.find(
         (node) => _isObject(node) && node.tag === 'url'
@@ -38,7 +40,7 @@ export function useWebsiteInfo(rawMessage: string): UseWebsiteInfoRet {
         return '';
       }
 
-      return urlTag.content.join('');
+      return getUrlTagRealUrl(urlTag); // 获取tag正确url的方式
     }
 
     return '';
