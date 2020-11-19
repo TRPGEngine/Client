0bdf1310f4647aaf0ca90938dc823b60fe91e390
moonrailgun <moonrailgun@gmail.com>
2020-11-19 21:47:55 +0800

    test: more test for layout tags

--- src/shared/components/layout/tags/__tests__/utils.spec.ts
+++ src/shared/components/layout/tags/__tests__/utils.spec.ts
@@ -1,4 +1,31 @@
-import { removePrivateProps } from '@shared/components/layout/tags/utils';
+import {
+  getOperationData,
+  removePrivateProps,
+  tryToNumber,
+} from '@shared/components/layout/tags/utils';
+
+describe('getOperationData', () => {
+  test.each([
+    ['a', { scope: 'data', field: 'a' }],
+    ['a.b', { scope: 'a', field: 'b' }],
+    ['a.b.c', { scope: 'a', field: 'b.c' }],
+    ['a.b.c.d', { scope: 'a', field: 'b.c.d' }],
+  ])('%s', (input, output) => {
+    expect(getOperationData(input)).toMatchObject(output);
+  });
+});
+
+describe('tryToNumber', () => {
+  test.each([
+    ['', ''],
+    [' 123', ' 123'],
+    ['123456789123456789', '123456789123456789'],
+    ['123456', 123456],
+    [123456, 123456],
+  ])('%s => %s', (input: any, output) => {
+    expect(tryToNumber(input)).toBe(output);
+  });
+});
 
 test('removePrivateProps', () => {
   const origin = {
