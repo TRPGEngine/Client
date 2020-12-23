47ecfe1ec441c537d7b9d9189654155892e12234
moonrailgun <moonrailgun@gmail.com>
2020-12-23 14:37:43 +0800

    chore: cypress

--- cypress/integration/web/login_page_spec.ts
+++ cypress/integration/web/login_page_spec.ts
@@ -3,5 +3,10 @@
 describe('The Login Page', () => {
   it('successfully loads', () => {
     cy.visit('/');
+
+    cy.url().should('include', '/entry/login');
+
+    // cy.get('input[placeholder=用户名]').type('admin8');
+    // cy.get('input[placeholder=密码]').type('admin8');
   });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,7 +19,6 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  require('@cypress/react/plugins/load-webpack')(on, config)
   // IMPORTANT to return the config object
   // with the any changed environment variables
   return config
