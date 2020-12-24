94a1595b31482673645de9268505371d4b0443c5
moonrailgun <moonrailgun@gmail.com>
2020-12-24 22:20:46 +0800

    refactor(web): loadable fallback

--- src/web/components/TLoadable.tsx
+++ src/web/components/TLoadable.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import loadable, {
   DefaultComponent,
   LoadableComponent,
-  OptionsWithoutResolver,
 } from '@loadable/component';
 import pMinDelay from 'p-min-delay';
+import { Loading } from './Loading';
 
 /**
  * 用法: Loadable(() => import('xxxxxx'))
  * @param loader 需要懒加载的组件
  */
 export function TLoadable<Props>(
-  loadFn: (props: Props) => Promise<DefaultComponent<Props>>,
-  options?: OptionsWithoutResolver<Props>
+  loadFn: (props: Props) => Promise<DefaultComponent<Props>>
 ): LoadableComponent<Props> {
-  return loadable((props) => pMinDelay(loadFn(props), 200));
+  return loadable((props) => pMinDelay(loadFn(props), 200), {
+    fallback: <Loading />,
+  });
 }
 
 export default TLoadable;
