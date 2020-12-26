2c75cb3a2fb4cefc6574a8011a83d16222dc8c88
moonrailgun <moonrailgun@gmail.com>
2020-12-26 23:13:24 +0800

    feat(portal): trpg news TRPG动态

--- src/portal/routes/trpg/news/index.tsx
+++ src/portal/routes/trpg/news/index.tsx
@@ -5,49 +5,75 @@ import { RouteComponentProps } from 'react-router-dom';
 import { useAsync } from 'react-use';
 import Parser from 'rss-parser';
 import _orderBy from 'lodash/orderBy';
-import { transformToDom } from './utils';
+import { Empty, List } from 'antd';
+import _flatten from 'lodash/flatten';
+import _isNil from 'lodash/isNil';
+import styled from 'styled-components';
+
 const parser = new Parser<RSSItem>();
 
-interface RSSItem {
+const Root = styled.div`
+  padding: 16px;
+`;
+
+interface RSSItem extends Parser.Output<{ [key: string]: any }> {
   title?: string;
 }
 
 interface Props extends RouteComponentProps {}
 const News: React.FC<Props> = React.memo((props) => {
   const { value, loading } = useAsync(async () => {
-    const list = await Promise.all(
-      config.url.rssNews.map(({ url }) =>
-        fetch(url)
-          .then((res) => res.text())
-          .then((xml) => parser.parseString(xml))
-          .catch(() => null)
-      )
-    );
+    try {
+      const list = await Promise.all(
+        config.url.rssNews.map(({ url }) =>
+          fetch(url)
+            .then((res) => res.text())
+            .then((xml) => parser.parseString(xml))
+            .catch(() => null)
+        )
+      );
 
-    return list.filter((item) => item !== null);
+      return _orderBy(
+        _flatten(
+          list
+            .filter((item): item is RSSItem => item !== null)
+            .map((item) => item.items)
+        ),
+        'isoDate',
+        'desc'
+      );
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
   }, []);
 
   if (loading) {
     return <Loading />;
   }
 
+  if (_isNil(value)) {
+    return <Empty />;
+  }
+
   return (
-    <div>
+    <Root>
       <h2>TRPG 动态</h2>
-      <div>
-        {value?.map((scope) =>
-          scope?.items.map((item, i) => {
-            return (
-              <div key={i}>
-                <p>{item.title}</p>
-                <p>{transformToDom(item.content ?? '')}</p>
-                <hr />
-              </div>
-            );
-          })
+      <List
+        itemLayout="horizontal"
+        dataSource={value}
+        renderItem={(item) => (
+          <a href={item.link} target="_blank" rel="noopener">
+            <List.Item>
+              <List.Item.Meta
+                title={item.title}
+                description={item.contentSnippet}
+              />
+            </List.Item>
+          </a>
         )}
-      </div>
-    </div>
+      />
+    </Root>
   );
 });
 News.displayName = 'News';
