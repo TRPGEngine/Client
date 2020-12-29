03aac794af08eb696f9a20b4e11f61ebdb35a3d0
moonrailgun <moonrailgun@gmail.com>
2020-12-29 22:39:38 +0800

    refactor: 增加useMsgHistory用于切换历史消息

---
+++ src/shared/hooks/useMsgHistory.ts
@@ -0,0 +1,54 @@
+import { useCallback, useState } from 'react';
+
+/**
+ * 消息历史
+ * 每个会话分别存储消息历史
+ * @param converseUUID 会话UUID
+ */
+const msgHistoryMap = new Map<string, string[]>();
+
+export function useMsgHistory(converseUUID: string) {
+  const [index, setIndex] = useState(-1);
+  const addHistoryMsg = useCallback(
+    (msg: string) => {
+      const arr = msgHistoryMap.get(converseUUID) ?? [];
+      arr.push(msg);
+      msgHistoryMap.set(converseUUID, arr);
+    },
+    [converseUUID]
+  );
+
+  const switchUp = useCallback((): string | null => {
+    const list = msgHistoryMap.get(converseUUID) ?? [];
+    if (list.length === 0) {
+      return null;
+    }
+    const newIndex = index >= list.length - 1 ? 0 : index + 1;
+    setIndex(newIndex);
+
+    return list[newIndex] ?? null;
+  }, [converseUUID, index]);
+
+  const switchDown = useCallback((): string | null => {
+    const list = msgHistoryMap.get(converseUUID) ?? [];
+    if (list.length === 0) {
+      return null;
+    }
+
+    const newIndex = index <= 0 ? list.length - 1 : index - 1;
+    setIndex(newIndex);
+
+    return list[newIndex] ?? null;
+  }, [converseUUID, index]);
+
+  const resetIndex = useCallback(() => {
+    setIndex(-1);
+  }, []);
+
+  return {
+    addHistoryMsg,
+    switchUp,
+    switchDown,
+    resetIndex,
+  };
+}
