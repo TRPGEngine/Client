f3740d9cdc5d01be515ffac6ea75ddbdb482950f
moonrailgun <moonrailgun@gmail.com>
2021-02-03 23:20:30 +0800

    style: 修改useToFunction的类型

--- src/shared/hooks/useToFunction.ts
+++ src/shared/hooks/useToFunction.ts
@@ -4,14 +4,14 @@ import { evalScript } from '@shared/components/layout/processor';
 /**
  * 确保传入的数据为数组
  */
-export function useToFunction(input: any): boolean {
+export function useToFunction(input: any): (...args: any[]) => {} | undefined {
   return useMemo(() => {
     if (typeof input === 'function') {
       return input;
     } else if (typeof input === 'string') {
       return evalScript(input, {});
     } else {
-      return () => {};
+      return undefined;
     }
   }, [input]);
 }
