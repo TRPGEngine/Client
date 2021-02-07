35be1b8eb03bf4e3ec934ed23cd03684c022d1fd
moonrailgun <moonrailgun@gmail.com>
2021-02-07 09:11:31 +0800

    fix(portal): 修复兔小巢portal跳转不正确的bug

--- src/portal/routes/help/txc.tsx
+++ src/portal/routes/help/txc.tsx
@@ -1,11 +1,11 @@
 import Loading from '@portal/components/Loading';
-import { checkToken } from '@portal/utils/auth';
+import { checkToken, getPortalJWTInfo } from '@portal/utils/auth';
 import { TMemo } from '@shared/components/TMemo';
 import config from '@shared/project.config';
-import { getJWTUserInfo } from '@shared/utils/jwt-helper';
 import { openPostWindow } from '@web/utils/dom-helper';
 import React from 'react';
 import { useAsync } from 'react-use';
+import _isNil from 'lodash/isNil';
 
 // 兔小巢反馈跳转
 
@@ -15,13 +15,21 @@ const TXCFeedback: React.FC = TMemo(() => {
 
     if (isLogin === true) {
       // 跳转到兔小巢
-      const userInfo = await getJWTUserInfo();
+      const userInfo = getPortalJWTInfo();
+      if (
+        _isNil(config.url.txcUrl) ||
+        _isNil(userInfo.uuid) ||
+        _isNil(userInfo.name)
+      ) {
+        return;
+      }
+
       openPostWindow({
         url: config.url.txcUrl,
         data: {
           openid: userInfo.uuid,
           nickname: userInfo.name,
-          avatar: userInfo.avatar,
+          avatar: userInfo.avatar ?? '',
         },
         target: '_self',
       });
