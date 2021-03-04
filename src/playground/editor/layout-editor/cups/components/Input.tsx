04012e0048d7cab861f9283ddf6e0e503be76212
moonrailgun <moonrailgun@gmail.com>
2021-03-04 22:02:03 +0800

    refactor(playground): 抽象出部分通用编辑组件

--- src/playground/editor/layout-editor/cups/components/Input.tsx
+++ src/playground/editor/layout-editor/cups/components/Input.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { regCup, useCurrentTeaId } from '@saucerjs/core';
+import { regCup } from '@saucerjs/core';
+import { Space } from 'antd';
+import { CommonEditorEditor, InputEditorEditor } from '../shared';
 import { TagInputEdit } from '@shared/components/layout/tags/Input/edit';
-import { useTeaAttrsContext } from '@saucerjs/editor';
-import { Checkbox, Input, Space } from 'antd';
-import { CupEditorWrapper } from '../shared';
 
 regCup({
   name: 'Input',
@@ -22,73 +21,11 @@ regCup({
     );
   },
   editor() {
-    const { currentTeaAttrs, setCurrentTeaAttrs } = useTeaAttrsContext();
-    const currentTeaId = useCurrentTeaId();
-
     return (
       <Space direction="vertical">
-        <CupEditorWrapper
-          title="组件名"
-          desc="组件名必须全局唯一, 会影响用户输入的数据的命名"
-        >
-          <Input
-            value={
-              currentTeaAttrs['name'] !== currentTeaId
-                ? currentTeaAttrs['name']
-                : undefined
-            }
-            placeholder={currentTeaId ?? undefined}
-            onChange={(e) =>
-              setCurrentTeaAttrs({
-                name: e.target.value || currentTeaId,
-              })
-            }
-          />
-        </CupEditorWrapper>
+        <CommonEditorEditor />
 
-        <CupEditorWrapper title="标签名">
-          <Input
-            value={currentTeaAttrs['label']}
-            onChange={(e) =>
-              setCurrentTeaAttrs({
-                label: e.target.value || undefined,
-              })
-            }
-          />
-        </CupEditorWrapper>
-
-        <CupEditorWrapper title="占位符">
-          <Input
-            value={currentTeaAttrs['placeholder']}
-            onChange={(e) =>
-              setCurrentTeaAttrs({
-                placeholder: e.target.value || undefined,
-              })
-            }
-          />
-        </CupEditorWrapper>
-
-        <CupEditorWrapper title="行内模式">
-          <Checkbox
-            checked={currentTeaAttrs['inline'] || false}
-            onChange={(e) =>
-              setCurrentTeaAttrs({
-                inline: e.target.checked || undefined,
-              })
-            }
-          />
-        </CupEditorWrapper>
-
-        <CupEditorWrapper title="隐藏标签">
-          <Checkbox
-            checked={currentTeaAttrs['hideLabel'] || false}
-            onChange={(e) =>
-              setCurrentTeaAttrs({
-                hideLabel: e.target.checked || undefined,
-              })
-            }
-          />
-        </CupEditorWrapper>
+        <InputEditorEditor />
       </Space>
     );
   },
--- src/playground/editor/layout-editor/cups/shared.tsx
+++ src/playground/editor/layout-editor/cups/shared.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { TMemo } from '@shared/components/TMemo';
-import { Space, Typography } from 'antd';
+import { Checkbox, Input, Space, Typography } from 'antd';
 import _isString from 'lodash/isString';
 import { TipIcon } from '@web/components/TipIcon';
+import { useCurrentTeaId } from '@saucerjs/core';
+import { useTeaAttrsContext } from '@saucerjs/editor';
 
 export const CupEditorWrapper: React.FC<{
   title: string;
@@ -23,3 +25,90 @@ export const CupEditorWrapper: React.FC<{
   );
 });
 CupEditorWrapper.displayName = 'CupEditorWrapper';
+
+/**
+ * 通用组件编辑设置
+ */
+export const CommonEditorEditor: React.FC = TMemo(() => {
+  const { currentTeaAttrs, setCurrentTeaAttrs } = useTeaAttrsContext();
+  const currentTeaId = useCurrentTeaId();
+
+  return (
+    <>
+      <CupEditorWrapper
+        title="组件名"
+        desc="组件名必须全局唯一, 会影响用户输入的数据的命名"
+      >
+        <Input
+          value={
+            currentTeaAttrs['name'] !== currentTeaId
+              ? currentTeaAttrs['name']
+              : undefined
+          }
+          placeholder={currentTeaId ?? undefined}
+          onChange={(e) =>
+            setCurrentTeaAttrs({
+              name: e.target.value || currentTeaId,
+            })
+          }
+        />
+      </CupEditorWrapper>
+
+      <CupEditorWrapper title="标签名">
+        <Input
+          value={currentTeaAttrs['label']}
+          onChange={(e) =>
+            setCurrentTeaAttrs({
+              label: e.target.value || undefined,
+            })
+          }
+        />
+      </CupEditorWrapper>
+    </>
+  );
+});
+
+/**
+ * 通用输入组件的编辑组件
+ */
+export const InputEditorEditor: React.FC = TMemo(() => {
+  const { currentTeaAttrs, setCurrentTeaAttrs } = useTeaAttrsContext();
+
+  return (
+    <>
+      <CupEditorWrapper title="占位符">
+        <Input
+          value={currentTeaAttrs['placeholder']}
+          onChange={(e) =>
+            setCurrentTeaAttrs({
+              placeholder: e.target.value || undefined,
+            })
+          }
+        />
+      </CupEditorWrapper>
+
+      <CupEditorWrapper title="行内模式">
+        <Checkbox
+          checked={currentTeaAttrs['inline'] || false}
+          onChange={(e) =>
+            setCurrentTeaAttrs({
+              inline: e.target.checked || undefined,
+            })
+          }
+        />
+      </CupEditorWrapper>
+
+      <CupEditorWrapper title="隐藏标签">
+        <Checkbox
+          checked={currentTeaAttrs['hideLabel'] || false}
+          onChange={(e) =>
+            setCurrentTeaAttrs({
+              hideLabel: e.target.checked || undefined,
+            })
+          }
+        />
+      </CupEditorWrapper>
+    </>
+  );
+});
+InputEditorEditor.displayName = 'InputEditorEditor';
