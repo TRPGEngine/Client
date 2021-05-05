d0cf2dba2f237dc477ee29dfbe152d8f31781509
moonrailgun <moonrailgun@gmail.com>
2021-05-05 22:23:12 +0800

    feat(web): 当@时没有内容的时候，自动根据uuid获取显示名

--- src/web/components/messageTypes/bbcode/AtTag.tsx
+++ src/web/components/messageTypes/bbcode/AtTag.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import type { TagProps } from '@shared/components/bbcode/type';
 import { TMemo } from '@shared/components/TMemo';
 import styled from 'styled-components';
+import { useUserName } from '@redux/hooks/user';
+import { t } from '@shared/i18n';
+
+const AutoNameAtTag: React.FC<{
+  userUUID: string;
+}> = TMemo((props) => {
+  const userName = useUserName(props.userUUID);
+  return <Root>@{userName === '' ? t('加载中...') : userName}</Root>;
+});
+AutoNameAtTag.displayName = 'AutoNameAtTag';
 
 const Root = styled.span`
   margin: 0 4px;
@@ -10,7 +20,13 @@ const Root = styled.span`
 `;
 export const AtTag: React.FC<TagProps> = TMemo((props) => {
   const { node } = props;
+  const attrs = node.attrs;
   const text = node.content.join('');
+  const uuid = attrs.uuid;
+
+  if (text.length === 0) {
+    return <AutoNameAtTag userUUID={uuid} />;
+  }
 
   return <Root>@{text}</Root>;
 });
