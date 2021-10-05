fc0c20ef0a15ffa89bb033db9d4990ceba14105b
moonrailgun <moonrailgun@gmail.com>
2021-10-05 11:43:56 +0800

    fix: #241 修复隐藏面板接收到web通知也会收到信息的bug

--- src/web/utils/notify.ts
+++ src/web/utils/notify.ts
@@ -5,6 +5,8 @@ import { switchMenuPannel } from '../../shared/redux/actions/ui';
 import { switchConverse } from '../../shared/redux/actions/chat';
 import _get from 'lodash/get';
 import type { TRPGStore } from '@redux/types/__all__';
+import { getAllHiddenGroupConverses } from '@redux/helpers/group';
+import { t } from '@shared/i18n';
 
 /**
  * 设置小红点
@@ -43,7 +45,7 @@ if (typeof visibilityChangeEvent === 'string') {
   document.addEventListener(visibilityChangeEvent, onVisibilityChange); // TODO: 需要在electron环境下测试是否可以运行
 }
 
-export default function (store: TRPGStore) {
+export function notifyBuilder(store: TRPGStore) {
   // 通过blur事件处理浏览器不是当前激活窗口的情况
   let isBlur = false;
   window.addEventListener('focus', () => {
@@ -54,6 +56,11 @@ export default function (store: TRPGStore) {
 
   return {
     onReceiveMessage(data) {
+      if (getAllHiddenGroupConverses(store).includes(data.converse_uuid)) {
+        // 如果是隐藏面板接受到的信息，则不启用通知
+        return;
+      }
+
       // web||electron通知
       const hidden = _get(window, 'document.hidden', false);
       if (hidden || isBlur) {
@@ -65,25 +72,28 @@ export default function (store: TRPGStore) {
         if (allowNotify && Notification.permission === 'granted') {
           // TODO: 这里在华为手机上会报个错误
           // Failed to construct 'Notification': Illegal constructor. Use ServiceWorkerRegistration.showNotification() instead.
-          const uuid = data.sender_uuid;
-          const userinfo = getUserInfoCache(data.sender_uuid);
+          const converseUUID = data.converse_uuid;
+          const senderUUID = data.sender_uuid;
+          const userinfo = getUserInfoCache(senderUUID);
           let username = userinfo.nickname || userinfo.username;
-          if (uuid && uuid.substr(0, 4) === 'trpg') {
-            username = '系统消息';
+          if (senderUUID && senderUUID.substr(0, 4) === 'trpg') {
+            username = t('系统消息');
           }
 
-          const notification = new Notification(`来自 ${username}:`, {
+          const notification = new Notification(`${t('来自')} ${username}:`, {
             body: data.message,
             icon: userinfo.avatar || config.defaultImg.trpgsystem,
             tag: 'trpg-msg',
             renotify: true,
-            data: { uuid },
+            data: { converseUUID },
           });
 
           notification.onclick = function () {
             window.focus();
+
+            // TODO: 这里似乎无效了，重写一下
             store.dispatch(switchMenuPannel(0));
-            store.dispatch(switchConverse(this.data.uuid));
+            store.dispatch(switchConverse(this.data.converseUUID));
           };
         }
 
